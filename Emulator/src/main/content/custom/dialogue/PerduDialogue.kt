a03d3007c1a976203a113d626f1f54bfe7da0967
szuma <szumaster2009@protonmail.com>
2025-03-17 19:54:58 +0100

    Fixed Perdu and Chris definitions

--- Emulator/src/main/content/custom/dialogue/PerduDialogue.kt
+++ Emulator/src/main/content/custom/dialogue/PerduDialogue.kt
@@ -8,16 +8,15 @@ import core.game.node.entity.player.Player
 import core.plugin.Initializable
 import core.tools.END_DIALOGUE
 import org.rs.consts.Components
-import org.rs.consts.NPCs
 
 @Initializable
-class SirVantDialogue(
+class PerduDialogue(
     player: Player? = null,
 ) : Dialogue(player) {
     override fun open(vararg args: Any): Boolean {
         npc = args[0] as NPC
         npcl(
-            FaceAnim.FRIENDLY,
+            FaceAnim.OLD_DEFAULT,
             "Hello. I've found some stuff that I think is yours. You can pay me with cash you're carrying. Would you like to buy anything from me?"
         )
         return true
@@ -38,16 +37,16 @@ class SirVantDialogue(
                 3 -> playerl(FaceAnim.HALF_ASKING, "Why do you charge for returning lost items?").also { stage++ }
                 4 -> playerl(FaceAnim.NEUTRAL, "About items that break when people kill me...").also { stage = 4 }
             }
-            2 -> npcl(FaceAnim.FRIENDLY, "You could get your stuff back elsewhere, sure. Now, maybe it'd be free, or maybe you'd have to pay someone, but you'd definitely have to go on a journey to get it back.").also { stage++ }
-            3 -> npcl(FaceAnim.FRIENDLY, "So I'm earning my fee by saving you a trip. Do you want to have a look?").also { stage = 0 }
+            2 -> npcl(FaceAnim.OLD_DEFAULT, "You could get your stuff back elsewhere, sure. Now, maybe it'd be free, or maybe you'd have to pay someone, but you'd definitely have to go on a journey to get it back.").also { stage++ }
+            3 -> npcl(FaceAnim.OLD_DEFAULT, "So I'm earning my fee by saving you a trip. Do you want to have a look?").also { stage = 0 }
             4 -> player(FaceAnim.HALF_ASKING, "Can you fix them?").also { stage++ }
-            5 -> npcl(FaceAnim.FRIENDLY, "For a fee, yes. Just show me the broken item.").also { stage = END_DIALOGUE }
-            6 -> npcl(FaceAnim.NEUTRAL, "Fair enough.").also { stage = END_DIALOGUE }
+            5 -> npcl(FaceAnim.OLD_DEFAULT, "For a fee, yes. Just show me the broken item.").also { stage = END_DIALOGUE }
+            6 -> npcl(FaceAnim.OLD_DEFAULT, "Fair enough.").also { stage = END_DIALOGUE }
         }
         return true
     }
 
     override fun getIds(): IntArray {
-        return intArrayOf(NPCs.SIR_VANT_7941)
+        return intArrayOf(8592)
     }
 }
