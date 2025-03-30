bc608918d32e818c63ba90b0360a42477c38c097
szuma <szumaster2009@protonmail.com>
2025-03-30 15:53:33 +0200

    Fixed creating tar

--- Emulator/src/main/content/global/skill/herblore/herbs/HerbTarListener.kt
+++ Emulator/src/main/content/global/skill/herblore/herbs/HerbTarListener.kt
@@ -7,6 +7,7 @@ import core.api.ui.repositionChild
 import core.game.dialogue.SkillDialogueHandler
 import core.game.interaction.IntType
 import core.game.interaction.InteractionListener
+import core.game.node.item.Item
 import org.rs.consts.Components
 import org.rs.consts.Items
 
@@ -17,7 +18,7 @@ class HerbTarListener : InteractionListener {
         onUseWith(IntType.ITEM, tar, Items.SWAMP_TAR_1939) { player, used, _ ->
             var tar = Tars.forId(used.id)
             val handler: SkillDialogueHandler =
-                object : SkillDialogueHandler(player, SkillDialogue.ONE_OPTION, tar!!.product) {
+                object : SkillDialogueHandler(player, SkillDialogue.ONE_OPTION, Item(tar!!.product)) {
                     override fun create(
                         amount: Int,
                         index: Int,
