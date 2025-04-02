59191220fa10772448cd24753e4b5c6272819c74
szuma <szumaster2009@protonmail.com>
2025-04-02 21:57:11 +0200

    Add CommandSystem docs, 90 iface to config

--- Emulator/src/main/core/game/system/command/CommandSystem.kt
+++ Emulator/src/main/core/game/system/command/CommandSystem.kt
@@ -1,23 +1,35 @@
 package core.game.system.command
 
 import core.game.node.entity.player.Player
-import core.game.node.entity.player.info.Rights
 import core.tools.colorize
 
+/**
+ * The command system responsible for parsing and executing player commands.
+ */
 class CommandSystem {
-    fun parse(
-        player: Player?,
-        message: String,
-    ): Boolean {
+    /**
+     * Parses a command message from the player and attempts to execute it.
+     *
+     * @param player The player issuing the command.
+     * @param message The command message as a string.
+     * @return `true` if the command was successfully handled or recognized, `false` otherwise.
+     */
+    fun parse(player: Player?, message: String): Boolean {
         player ?: return false
         val arguments = message.split(" ").toTypedArray()
         val command = CommandMapping.get(arguments[0])
 
-        if (command == null && (player.rights != Rights.REGULAR_PLAYER)) {
-            player.sendMessage(colorize("-->%R${arguments[0]}: command not found."))
+        if (command == null) {
+            for (set in CommandSet.values()) {
+                if (set.interpret(player, arguments[0], *arguments)) {
+                    player.sendMessage(colorize("-->%Y${arguments[0]}: Deprecated command"))
+                    return true
+                }
+            }
+            player.sendMessage(colorize("-->%R${arguments[0]}: command not found"))
         } else {
             try {
-                command?.attemptHandling(player, arguments)
+                command.attemptHandling(player, arguments)
             } catch (e: IllegalStateException) {
                 return true
             }
@@ -26,6 +38,9 @@ class CommandSystem {
     }
 
     companion object {
+        /**
+         * Singleton instance of the command system.
+         */
         val commandSystem = CommandSystem()
     }
 }
