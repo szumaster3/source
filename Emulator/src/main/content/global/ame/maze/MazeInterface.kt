5c81848baea938bc464153f22257db550da10610
szuma <szumaster2009@protonmail.com>
2025-04-07 18:09:15 +0200

    Removed that idea

--- Emulator/src/main/content/global/ame/maze/MazeInterface.kt
+++ Emulator/src/main/content/global/ame/maze/MazeInterface.kt
@@ -5,17 +5,12 @@ import core.api.*
 import core.api.ui.restoreTabs
 import core.game.event.EventHook
 import core.game.event.TickEvent
-import core.game.interaction.DestinationFlag
 import core.game.interaction.InteractionListener
-import core.game.interaction.MovementPulse
 import core.game.node.entity.Entity
-import core.game.node.entity.impl.PulseType
 import core.game.node.entity.player.Player
-import core.game.system.timer.impl.AntiMacro
 import core.game.world.map.Location
 import core.game.world.map.zone.ZoneBorders
 import core.game.world.map.zone.ZoneRestriction
-import org.rs.consts.NPCs
 
 /**
  * Maze interface.
@@ -46,41 +41,19 @@ class MazeInterface : InteractionListener, EventHook<TickEvent>, MapArea {
         if (entity is Player) {
             val player = entity.asPlayer()
             player.hook(Event.Tick, this)
-            val follower = core.game.node.entity.npc.NPC.create(
-                NPCs.MYSTERIOUS_OLD_MAN_410, Location.getRandomLocation(player.location, 1, true)
-            )
-            follower.init()
         }
     }
 
     override fun areaLeave(entity: Entity, logout: Boolean) {
         if (entity is Player) {
             val player = entity.asPlayer()
-            val follower = AntiMacro.getEventNpc(player)
             closeOverlay(player)
             restoreTabs(player)
             player.unhook(this)
-            follower!!.clear()
         }
     }
 
     override fun entityStep(entity: Entity, location: Location, lastLocation: Location) {
-        if (entity is Player) {
-            val player = entity.asPlayer()
-            val follower = AntiMacro.getEventNpc(player)
-            // if (!withinDistance(player, follower!!.location, 3))
-            //     follower.teleporter?.send(player.location)
-            // else
-            follower!!.pulseManager.run(
-                (object : MovementPulse(follower, player, DestinationFlag.FOLLOW_ENTITY) {
-                    override fun pulse(): Boolean {
-                        follower.face(player)
-                        return false
-                    }
-                }),
-                PulseType.STANDARD,
-            )
-        }
     }
 
     override fun process(entity: Entity, event: TickEvent) {
