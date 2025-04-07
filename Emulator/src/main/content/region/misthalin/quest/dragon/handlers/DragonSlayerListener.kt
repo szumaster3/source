40261727457167bf05c70b50d7bdf945eaf19302
szuma <szumaster2009@protonmail.com>
2025-04-07 08:37:03 +0200

    Small adjustment for Dragon slayer

--- Emulator/src/main/content/region/misthalin/quest/dragon/handlers/DragonSlayerListener.kt
+++ Emulator/src/main/content/region/misthalin/quest/dragon/handlers/DragonSlayerListener.kt
@@ -3,76 +3,91 @@ package content.region.misthalin.quest.dragon.handlers
 import content.region.misthalin.quest.dragon.DragonSlayer
 import core.api.*
 import core.api.quest.getQuestStage
+import core.api.quest.isQuestComplete
 import core.game.global.action.ClimbActionHandler
 import core.game.interaction.IntType
 import core.game.interaction.InteractionListener
 import core.game.node.entity.player.link.diary.DiaryType
 import core.game.world.map.Location
-import core.game.world.map.RegionManager.getLocalNpcs
+import core.game.world.map.RegionManager
 import core.game.world.update.flag.context.Animation
 import org.rs.consts.*
 
 class DragonSlayerListener : InteractionListener {
+
     override fun defineListeners() {
+        /*
+         * Handles interaction with the locked cell door.
+         */
+
         on(Scenery.CELL_DOOR_40184, IntType.SCENERY, "open") { player, _ ->
             sendMessage(player, "It's locked tight.")
             return@on true
         }
 
+        /*
+         * Handles interaction with Wormbrain.
+         * Adjusts the player's location based on their proximity to the NPC.
+         */
+
         setDest(IntType.NPC, intArrayOf(NPCs.WORMBRAIN_745), "talk-to") { player, node ->
             val npcLocation = node.asNpc().location
             val playerLocation = player.asPlayer().location
-
             val isWithinDistance = playerLocation.withinMaxnormDistance(npcLocation, 1)
             val targetLocation = if (isWithinDistance) playerLocation else npcLocation
-
             return@setDest Location.create(targetLocation.x, targetLocation.y, 0)
         }
 
+        /*
+         * Handles diary task.
+         */
+
         on(Scenery.CLIMBING_ROPE_25213, IntType.SCENERY, "climb") { player, _ ->
-            val animation = Animation(Animations.USE_LADDER_828)
-            val location = Location(2834, 3258, 0)
-
-            ClimbActionHandler.climb(player, animation, location)
+            ClimbActionHandler.climb(player, Animation(Animations.USE_LADDER_828), Location(2834, 3258, 0))
             finishDiaryTask(player, DiaryType.KARAMJA, 1, 2)
-
             return@on true
         }
+
+        /*
+         * Handles entrance from crandor to elvarg dungeon.
+         */
 
         on(Scenery.HOLE_25154, IntType.SCENERY, "enter") { player, _ ->
-            val location = Location(2833, 9658, 0)
-            ClimbActionHandler.climb(player, null, location)
+            ClimbActionHandler.climb(player, null, Location(2833, 9658, 0))
             return@on true
         }
+
+        /*
+         * Handles the quest NPC interaction (Dragon slayer).
+         */
 
         on(NPCs.ELVARG_742, IntType.NPC, "attack") { player, node ->
-            if (getQuestStage(player, Quests.DRAGON_SLAYER) == 40 && inInventory(player, DragonSlayer.ELVARG_HEAD.id)) {
-                sendMessage(player, "You have already slain the dragon. Now you just need to return to Oziach for")
-                sendMessage(player, "your reward!")
-                return@on true
+            when {
+                getQuestStage(player, Quests.DRAGON_SLAYER) == 40 && inInventory(player, DragonSlayer.ELVARG_HEAD.id) -> {
+                    sendMessage(player, "You have already slain the dragon. Now you just need to return to Oziach for your reward!")
+                }
+                getQuestStage(player, Quests.DRAGON_SLAYER) > 40 -> {
+                    sendMessage(player, "You have already slain Elvarg the dragon.")
+                }
+                else -> {
+                    player.attack(node)
+                    face(player, node, 3)
+                }
             }
-            if (getQuestStage(player, Quests.DRAGON_SLAYER) > 40) {
-                sendMessage(player, "You have already slain Elvarg the dragon.")
-                return@on true
-            }
-
-            player.attack(node)
-            face(player, node, 3)
             return@on true
         }
+
+        /*
+         * Handles climbing over a wall to elvarg NPC.
+         */
 
         on(Scenery.WALL_25161, IntType.SCENERY, "climb-over") { player, _ ->
             if (player.location.x >= 2847) {
-                if (getQuestStage(player, Quests.DRAGON_SLAYER) == 40 &&
-                    !inInventory(
-                        player,
-                        DragonSlayer.ELVARG_HEAD.id,
-                    )
-                ) {
+                if (getQuestStage(player, Quests.DRAGON_SLAYER) == 40 && !inInventory(player, DragonSlayer.ELVARG_HEAD.id)) {
                     if (player.location.x <= 2845) {
-                        val npcs = getLocalNpcs(player)
+                        val npcs = RegionManager.getLocalNpcs(player)
                         for (n in npcs) {
-                            if (n.id == 742) {
+                            if (n.id == NPCs.ELVARG_742) {
                                 n.properties.combatPulse.attack(player)
                                 return@on true
                             }
@@ -86,59 +101,51 @@ class DragonSlayerListener : InteractionListener {
                     10,
                     60,
                     null,
-                    Animations.CLIMB_OVER_ROCK_10573,
+                    Animations.CLIMB_OVER_ROCK_10573
                 )
                 return@on true
             }
-            if (getQuestStage(player, Quests.DRAGON_SLAYER) == 40 && inInventory(player, DragonSlayer.ELVARG_HEAD.id)) {
-                sendMessage(player, "You have already slain the dragon. Now you just need to return to Oziach for")
-                sendMessage(player, "your reward!")
+            if (inInventory(player, DragonSlayer.ELVARG_HEAD.id)) {
+                sendMessage(player, "You have already slain the dragon. Now you just need to return to Oziach for your reward!")
                 return@on true
             }
-            if (getQuestStage(player, Quests.DRAGON_SLAYER) > 40) {
+            if (isQuestComplete(player, Quests.DRAGON_SLAYER)) {
                 sendMessage(player, "You have already slain Elvarg the dragon.")
                 return@on true
             }
-
             return@on true
         }
 
+        /*
+         * Handles creating a crandor map from pieces.
+         */
+
         onUseWith(IntType.ITEM, mapPieces, *mapPieces) { player, _, _ ->
-            if (!inInventory(player, Items.MAP_PART_1537) ||
-                !inInventory(player, Items.MAP_PART_1536) ||
-                !inInventory(
-                    player,
-                    Items.MAP_PART_1535,
-                )
-            ) {
+            if (!anyInInventory(player, Items.MAP_PART_1537, Items.MAP_PART_1536, Items.MAP_PART_1535)) {
                 sendMessage(player, "You don't have all the map pieces yet.")
-            } else if (removeItem(player, Items.MAP_PART_1537) &&
-                removeItem(player, Items.MAP_PART_1536) &&
-                removeItem(
-                    player,
-                    Items.MAP_PART_1535,
-                )
-            ) {
-                sendItemDialogue(
-                    player,
-                    Items.CRANDOR_MAP_1538,
-                    "You put the three pieces together and assemble a map that shows the route through the reefs to Crandor.",
-                )
-                addItem(player, Items.CRANDOR_MAP_1538, 1)
+                return@onUseWith false
             }
+            if (!player.inventory.removeAll(mapPieces)) return@onUseWith false
+
+            sendItemDialogue(
+                player,
+                Items.CRANDOR_MAP_1538,
+                "You put the three pieces together and assemble a map that shows the route through the reefs to Crandor."
+            )
+            addItem(player, Items.CRANDOR_MAP_1538, 1)
             return@onUseWith true
         }
 
+        /*
+         * Handles interaction for each map pieces.
+         */
+
         on(mapPieces + Items.CRANDOR_MAP_1538, IntType.ITEM, "study") { player, node ->
-            val itemDialogues =
-                mapOf(
-                    Items.MAP_PART_1535 to
-                        "This is a piece of map that you found in Melzar's Maze. You will need to join it to the other two map pieces before you can see the route to Crandor.",
-                    Items.MAP_PART_1536 to
-                        "This is a piece of map that you got from Wormbrain, the goblin thief. You will need to join it to the other two map pieces before you can see the route to Crandor.",
-                    Items.MAP_PART_1537 to
-                        "This is a piece of map that you found in a secret chest in the Dwarven Mine. You will need to join it to the other two map pieces before you can see the route to Crandor.",
-                )
+            val itemDialogues = mapOf(
+                Items.MAP_PART_1535 to "This is a piece of map that you found in Melzar's Maze. You will need to join it to the other two map pieces before you can see the route to Crandor.",
+                Items.MAP_PART_1536 to "This is a piece of map that you got from Wormbrain, the goblin thief. You will need to join it to the other two map pieces before you can see the route to Crandor.",
+                Items.MAP_PART_1537 to "This is a piece of map that you found in a secret chest in the Dwarven Mine. You will need to join it to the other two map pieces before you can see the route to Crandor."
+            )
 
             itemDialogues[node.id]?.let { dialogue ->
                 sendItemDialogue(player, node.id, dialogue)
