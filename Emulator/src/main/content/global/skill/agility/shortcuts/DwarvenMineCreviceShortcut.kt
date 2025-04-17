0788fa9618215149f6c7126d13b8a209874188f4
szuma <szumaster2009@protonmail.com>
2025-04-17 02:58:12 +0200

    Improved Dwarven mine crevice shortcut

--- Emulator/src/main/content/global/skill/agility/shortcuts/DwarvenMineCreviceShortcut.kt
+++ Emulator/src/main/content/global/skill/agility/shortcuts/DwarvenMineCreviceShortcut.kt
@@ -1,7 +1,6 @@
 package content.global.skill.agility.shortcuts
 
-import core.api.hasLevelDyn
-import core.api.sendDialogue
+import core.api.*
 import core.game.interaction.IntType
 import core.game.interaction.InteractionListener
 import core.game.node.entity.impl.ForceMovement
@@ -15,7 +14,7 @@ import org.rs.consts.Scenery
 
 class DwarvenMineCreviceShortcut : InteractionListener {
     override fun defineListeners() {
-        on(Scenery.CREVICE_30868, IntType.SCENERY, "squeeze-through") { player, _ ->
+        on(Scenery.CREVICE_30868, IntType.SCENERY, "squeeze-through") { player, node ->
             if (!hasLevelDyn(player, Skills.AGILITY, 42)) {
                 sendDialogue(player, "You need an agility level of at least 42 to do this.")
                 return@on true
@@ -28,19 +27,13 @@ class DwarvenMineCreviceShortcut : InteractionListener {
                     Location(3035, 9806, 0)
                 }
 
-            player.animate(Animation(Animations.DUCK_UNDER_2240))
-            val movement = ForceMovement(player, player.location, destination, Animation(Animations.DUCK_UNDER_2240))
-            movement.run(player, 8)
-
-            Pulser.submit(
-                object : Pulse(7, player) {
-                    override fun pulse(): Boolean {
-                        player.animate(Animation(Animations.DUCK_UNDER_2240))
-                        return true
-                    }
-                },
-            )
-
+            lock(player, 8)
+            face(player, node)
+            animate(player, Animations.DUCK_UNDER_2240)
+            forceMove(player, player.location, destination, 0, 240, null, Animations.HUMAN_TURNS_INVISIBLE_2590)
+            runTask(player, 7) {
+                animate(player, Animations.DUCK_UNDER_2240)
+            }
             return@on true
         }
     }
