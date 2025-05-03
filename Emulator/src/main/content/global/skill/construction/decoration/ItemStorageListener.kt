60fe1fd2995cc5348ad2800457a1740376a14cbe
szumaster <szumaster2009@protonmail.com>
2025-05-03 09:46:52 +0200

    Cleanup item storage

--- Emulator/src/main/content/global/skill/construction/decoration/ItemStorageListener.kt
+++ Emulator/src/main/content/global/skill/construction/decoration/ItemStorageListener.kt
@@ -2,13 +2,12 @@ package content.global.skill.construction.decoration
 
 import core.api.*
 import core.api.item.allInInventory
-import core.api.ui.sendInterfaceConfig
+import core.cache.def.impl.ItemDefinition
 import core.game.interaction.IntType
 import core.game.interaction.InteractionListener
 import core.game.interaction.InterfaceListener
 import core.game.node.entity.player.Player
 import core.game.node.item.Item
-import core.game.world.GameWorld
 import core.net.packet.PacketRepository
 import core.net.packet.context.ContainerContext
 import core.net.packet.out.ContainerPacket
@@ -28,13 +27,14 @@ class ItemStorageListener : InterfaceListener, InteractionListener {
         on(INTERFACE) { player, _, _, buttonID, _, _ ->
             val setIndex = (buttonID - 56) / 2
             if (getAttribute(player, "con:bookcase", false)) {
-                addItem(player, getBook(setIndex))
+                if (setIndex in BOOKCASE_CONTENT.indices) {
+                    val itemId = BOOKCASE_CONTENT[setIndex]
+                    addItem(player, itemId)
+                } else {
+                    player.debug("Invalid book index.")
+                }
             } else if (getAttribute(player, "con:fancy-dress-box", false)) {
                 if (setIndex in DRESS_BOX_CONTENT.indices) {
-                    // val componentID = (166..224 step 2)
-                    // for (i in componentID) {
-                    //     sendInterfaceConfig(player, INTERFACE, i, false)
-                    // }
                     toggleFancyDressSet(player, setIndex)
                 } else {
                     player.debug("Invalid costume set.")
@@ -100,10 +100,12 @@ class ItemStorageListener : InterfaceListener, InteractionListener {
             setAttribute(player, "con:bookcase", true)
             openInterface(player, INTERFACE).also {
                 sendString(player, "Bookcase", INTERFACE, 225)
-                BOOKCASE_CONTENT.forEachIndexed { index, (itemId, description) ->
-                    sendString(player, "$YELLOW${getItemName(itemId)}</col> <br>$description", INTERFACE, 55 + index * 2)
+                BOOKCASE_CONTENT.forEachIndexed { index, itemId ->
+                    val itemName = getItemName(itemId)
+                    val itemExamine = ItemDefinition.forId(itemId).examine
+                    sendString(player, "$YELLOW$itemName</col> <br>$itemExamine", INTERFACE, 55 + index * 2)
                 }
-                PacketRepository.send(ContainerPacket::class.java, ContainerContext(player, INTERFACE, 164, 30, BOOKCASE_CONTENT.map { Item(it.first) }.toTypedArray(), false))
+                PacketRepository.send(ContainerPacket::class.java, ContainerContext(player, INTERFACE, 164, 30, BOOKCASE_CONTENT.map { Item(it) }.toTypedArray(), false))
             }
             return@on true
         }
@@ -128,36 +130,8 @@ class ItemStorageListener : InterfaceListener, InteractionListener {
         private val DRESS_BOX_CONTENT = intArrayOf(Items.MIME_MASK_10629, Items.PRINCESS_BLOUSE_10630, Items.FROG_MASK_10721, Items.ZOMBIE_SHIRT_10631, Items.CAMO_TOP_10632, Items.LEDERHOSEN_TOP_10633, Items.SHADE_ROBE_10634)
         // Bookcase.
         private val BOOKCASE = intArrayOf(Scenery.BOOKCASE_13597, Scenery.BOOKCASE_13598, Scenery.BOOKCASE_13599)
-        private val BOOKCASE_CONTENT = listOf(Items.ARENA_BOOK_6891 to "Magic Training Arena Lore Book", Items.MY_NOTES_11339 to "Discoveries from the ancient cavern beneath the lake", Items.CRUMBLING_TOME_4707 to "Legend of the Brothers", Items.PIE_RECIPE_BOOK_7162 to "Pie Recipes", Items.GIANNES_COOK_BOOK_2167 to "The collected recipes of Aluft Gianne", Items.GAME_BOOK_7681 to "Party Pete's Bumper Book of Games", Items.STRONGHOLD_NOTES_9004 to "Stronghold of Security - Notes", Items.COCKTAIL_GUIDE_2023 to "The Blurberry Cocktail Guide", Items.TARNS_DIARY_10587 to "The Diary of Tarn Razorlor", Items.CONSTRUCTION_GUIDE_8463 to "Guide to Construction", Items.GLASSBLOWING_BOOK_11656 to "Ultimate Guide to Glassblowing", Items.BREWIN_GUIDE_8989 to "Brewin' Guide", Items.SECURITY_BOOK_9003 to "${GameWorld.settings?.name} Account Security", Items.QUEEN_HELP_BOOK_10562 to "Queen Help", Items.ABYSSAL_BOOK_5520 to "Abyssal Research Notes", Items.EXPLORERS_NOTES_11677 to "Beyond Trollheim", Items.GOBLIN_BOOK_10999 to "The Book of the Big High War God (Another Slice of H.A.M.)", Items.DWARVEN_LORE_4568 to "The Arzinian Being of Bordanzan (Between a Rock...)", Items.BOOK_O_PIRACY_7144 to "The Little Book o' Piracy (Cabin Fever)", Items.CLOCKWORK_BOOK_10594 to "Clockwork Toys - Chapter 1.0 (Cold War)", Items.SCABARITE_NOTES_11975 to "'My Notes' (Dealing with Scabaras)", Items.TRANSLATION_4655 to "Translation Primer (Desert Treasure)", Items.BOOK_ON_CHEMICALS_711 to "Volatile Chemicals (Digsite)", Items.INSTRUCTION_MANUAL_5 to "Dwarf Multicannon Manual (Dwarf Cannon)", Items.BIRD_BOOK_10173 to "William Oddity's Guide to the Avian (Eagles' Peak)", Items.FEATHERED_JOURNAL_10179 to "The Journal of Arthur Artimus (Eagles' Peak)", Items.BATTERED_BOOK_2886 to "Book of the Elemental Shield (Elemental Workshop I)", Items.BEATEN_BOOK_9717 to "Book of the Elemental Helm (Elemental Workshop II)", Items.A_HANDWRITTEN_BOOK_9627 to "Crystal Singing for Beginners (The Eyes of Glouphrie)", Items.VARMENS_NOTES_4616 to "The Ruins of Uzer (The Golem)")
+        private val BOOKCASE_CONTENT = intArrayOf(Items.ARENA_BOOK_6891, Items.MY_NOTES_11339, Items.CRUMBLING_TOME_4707, Items.PIE_RECIPE_BOOK_7162, Items.GIANNES_COOK_BOOK_2167, Items.GAME_BOOK_7681, Items.STRONGHOLD_NOTES_9004, Items.COCKTAIL_GUIDE_2023, Items.TARNS_DIARY_10587, Items.CONSTRUCTION_GUIDE_8463, Items.GLASSBLOWING_BOOK_11656, Items.BREWIN_GUIDE_8989, Items.SECURITY_BOOK_9003, Items.QUEEN_HELP_BOOK_10562, Items.ABYSSAL_BOOK_5520, Items.EXPLORERS_NOTES_11677, Items.GOBLIN_BOOK_10999, Items.DWARVEN_LORE_4568, Items.BOOK_O_PIRACY_7144, Items.CLOCKWORK_BOOK_10594, Items.SCABARITE_NOTES_11975, Items.TRANSLATION_4655, Items.BOOK_ON_CHEMICALS_711, Items.INSTRUCTION_MANUAL_5, Items.BIRD_BOOK_10173, Items.FEATHERED_JOURNAL_10179, Items.BATTERED_BOOK_2886, Items.BEATEN_BOOK_9717, Items.A_HANDWRITTEN_BOOK_9627, Items.VARMENS_NOTES_4616)
         private val BOOKCASE_RESTRICTED_CONTENT = intArrayOf(Items.HOLY_BOOK_3840, Items.DAMAGED_BOOK_3839, Items.BOOK_OF_BALANCE_3844, Items.DAMAGED_BOOK_3843, Items.UNHOLY_BOOK_3842, Items.DAMAGED_BOOK_3841, Items.STRANGE_BOOK_5507, Items.BOOK_ON_CHICKENS_7464, Items.BOOK_OF_FOLKLORE_5508, Items.PVP_WORLDS_MANUAL_14056)
-
-        /**
-         * Retrieves the first element of a book's details based on the provided id.
-         *
-         * @param id The book item id.
-         * @return The first element if the `id` is valid; otherwise, returns `0`.
-         */
-        private fun getBook(id: Int): Int =
-            if (id in 56..114 step 2) {
-                BOOKCASE_CONTENT[(id - 56) / 2].first
-            } else {
-                0
-            }
-
-        /**
-         * Generates an element based on the given `id` and the provided `items` array.
-         *
-         * @param id The identifier of the item to be generated.
-         * @param items The `IntArray` containing the data used to generate the item.
-         * @return The value from the `items` array corresponding to the given `id`, or `0` if the `id` is out of range.
-         */
-        private fun getItem(id: Int, items: IntArray): Int {
-            return if (id in 56..114 step 2) {
-                items[(id - 56) / 2]
-            } else {
-                0
-            }
-        }
 
         /**
          * Toggles a fancy sets.
