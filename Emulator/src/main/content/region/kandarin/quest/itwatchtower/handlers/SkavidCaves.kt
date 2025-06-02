7c1be40d425a7e45afdfdf729b08d5fbb01b7595
szumaster <szumaster2009@protonmail.com>
2025-06-02 10:39:26 +0200

    Map area for `SkavidCaves`, npc configs, skavid npc spawns

---
+++ Emulator/src/main/content/region/kandarin/quest/itwatchtower/handlers/SkavidCaves.kt
@@ -0,0 +1,27 @@
+package content.region.kandarin.quest.itwatchtower.handlers
+
+import core.api.MapArea
+import core.game.node.entity.Entity
+import core.game.node.entity.player.Player
+import core.game.world.map.Location
+import core.game.world.map.zone.ZoneBorders
+import core.game.world.map.zone.ZoneRestriction
+
+class SkavidCaves : MapArea {
+
+    override fun defineAreaBorders(): Array<ZoneBorders> {
+        return arrayOf(ZoneBorders(2496, 9408, 2559, 9471))
+    }
+
+    override fun getRestrictions(): Array<ZoneRestriction> {
+        return arrayOf(ZoneRestriction.CANNON)
+    }
+
+    override fun entityStep(entity: Entity, location: Location, lastLocation: Location) {
+        if(entity is Player) if (entity.settings.runEnergy < 100.0) {
+            entity.settings.updateRunEnergy(-100.0)
+        }
+
+    }
+
+}
