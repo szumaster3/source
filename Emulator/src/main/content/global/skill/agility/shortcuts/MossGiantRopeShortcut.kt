90e2dc4a3449c2abd51f179edeedeac89bbbebf8
szumaster <szumaster2009@protonmail.com>
2025-06-06 02:27:03 +0200

    Fixed MossGiantRopeShortcut

--- Emulator/src/main/content/global/skill/agility/shortcuts/MossGiantRopeShortcut.kt
+++ Emulator/src/main/content/global/skill/agility/shortcuts/MossGiantRopeShortcut.kt
@@ -2,9 +2,8 @@ package content.global.skill.agility.shortcuts
 
 import content.global.skill.agility.AgilityHandler
 import content.global.skill.agility.AgilityShortcut
-import core.api.animateScenery
-import core.api.finishDiaryTask
-import core.api.playAudio
+import core.api.*
+import core.game.node.Node
 import core.game.node.entity.player.Player
 import core.game.node.entity.player.link.diary.DiaryType
 import core.game.node.scenery.Scenery
@@ -22,17 +21,16 @@ class MossGiantRopeShortcut : AgilityShortcut(intArrayOf(2322, 2323), 10, 0.0, "
 
     override fun run(player: Player, scenery: Scenery, option: String, failed: Boolean) {
         if (!player.location.withinDistance(scenery.location, 4)) {
-            player.sendMessage("I can't reach that.")
+            sendMessage(player, "I can't reach that.")
             return
         }
 
-        playAudio(player, Sounds.SWING_ACROSS_2494)
-
         val end = when (scenery.id) {
-            2322 -> Location(2704, 3209)
-            else -> Location(2709, 3205)
+            2322 -> Location.create(2704, 3209, 0)
+            else -> Location.create(2709, 3205, 0)
         }
 
+        playAudio(player, Sounds.SWING_ACROSS_2494)
         animateScenery(player, scenery, 497, true)
 
         AgilityHandler.forceWalk(
@@ -47,6 +45,13 @@ class MossGiantRopeShortcut : AgilityShortcut(intArrayOf(2322, 2323), 10, 0.0, "
             1
         )
 
-        finishDiaryTask(player, DiaryType.KARAMJA, 0, 1)
+        runTask(player, 1) {
+            finishDiaryTask(player, DiaryType.KARAMJA, 0, 1)
+        }
     }
+
+    override fun getDestination(node: Node?, n: Node): Location {
+        return if (n.id == 2322) Location.create(2709, 3209, 0) else Location.create(2705, 3205, 0)
+    }
+
 }
