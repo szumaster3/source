6d31127e6e29532781ec7d7c185749c761fed920
szumaster <szumaster2009@protonmail.com>
2025-06-11 13:25:58 +0200

    Small update for KeldagrimMinecartListener

--- Emulator/src/main/content/global/travel/KeldagrimMinecartListener.kt
+++ Emulator/src/main/content/global/travel/KeldagrimMinecartListener.kt
@@ -5,6 +5,7 @@ import core.api.*
 import core.api.quest.isQuestComplete
 import core.api.ui.closeDialogue
 import core.api.ui.setMinimapState
+import core.game.dialogue.SequenceDialogue.dialogue
 import core.game.interaction.IntType
 import core.game.interaction.InteractionListener
 import core.game.node.entity.npc.NPC
@@ -18,12 +19,14 @@ import org.rs.consts.Quests
 import org.rs.consts.Scenery
 
 /**
- * Handles Keldagrim minecart system.
+ * Handles Keldagrim minecart travel system interactions.
  */
 class KeldagrimMinecartListener : InteractionListener {
 
-    private val hiddenTrapdoor = Scenery.HIDDEN_TRAPDOOR_28094
-    private val minecarts = intArrayOf(Scenery.TRAIN_CART_7028, Scenery.TRAIN_CART_7029, Scenery.TRAIN_CART_7030)
+    private val optionsWithQuest = arrayOf("To the Grand Exchange", "To Ice Mountain", "To White Wolf Mountain", "Stay here")
+    private val optionsWithoutQuest = arrayOf("To the Grand Exchange", "To Ice Mountain", "Stay here")
+
+    private val sceneryId = intArrayOf(Scenery.HIDDEN_TRAPDOOR_28094, Scenery.TRAIN_CART_7028, Scenery.TRAIN_CART_7029, Scenery.TRAIN_CART_7030)
     private val destinations = mapOf(
         "GE" to Location.create(3140, 3507, 0),
         "Mine" to Location.create(2997, 9837, 0),
@@ -31,87 +34,53 @@ class KeldagrimMinecartListener : InteractionListener {
     )
 
     override fun defineListeners() {
-        on(hiddenTrapdoor, IntType.SCENERY, "open") { player, _ ->
-            if (player.hasMinecartUnlock()) showTrapdoorDialogue(player)
-            else sendDialogue(player, "You must visit Keldagrim to use this shortcut.")
-            return@on true
-        }
+        on(sceneryId, IntType.SCENERY, "open", "ride") { player, node ->
+            val questDone = isQuestComplete(player, Quests.FISHING_CONTEST)
+            val options = if (questDone) optionsWithQuest else optionsWithoutQuest
 
-        on(minecarts, IntType.SCENERY, "ride") { player, node ->
-            if (!player.hasMinecartUnlock()) {
-                sendDialogue(player, "You must visit Keldagrim to use this shortcut.")
-            } else if (node.id == Scenery.TRAIN_CART_7028) {
-                showKeldagrimExitOptions(player)
-            } else {
-                showKeldagrimEntryOptions(player)
+            if (!getAttribute(player, GameAttributes.MINECART_TRAVEL_UNLOCK, false)) {
+                sendMessage(player, "You must visit Keldagrim to use this shortcut.")
+                return@on true
+            }
+
+            dialogue(player) {
+                if (node.id == Scenery.HIDDEN_TRAPDOOR_28094) {
+                    message("This trapdoor leads to a small dwarven mine cart station. The mine", "cart will take you to Keldagrim.")
+                }
+                if (node.id == Scenery.TRAIN_CART_7028) {
+                    options("What would you like to do?", *options) { choice ->
+                        when {
+                            choice == options.size -> closeDialogue(player)
+                            choice == 1 -> startTravelFromKeldagrim(player, destinations.getValue("GE"))
+                            choice == 2 -> startTravelFromKeldagrim(player, destinations.getValue("Mine"))
+                            questDone && choice == 3 -> startTravelFromKeldagrim(player, destinations.getValue("Sindarpos"))
+                            else -> closeDialogue(player)
+                        }
+                    }
+                } else {
+                    options("What would you like to do?", "Travel to Keldagrim", "Stay here") { choice ->
+                        if (choice == 1) startTravelToKeldagrim(player) else closeDialogue(player)
+                    }
+                }
             }
             return@on true
         }
-    }
 
-    private fun Player.hasMinecartUnlock() =
-        getAttribute(GameAttributes.MINECART_TRAVEL_UNLOCK, false)
-
-    private fun showTrapdoorDialogue(player: Player) {
-        sendDialogueLines(player,
-            "This trapdoor leads to a small dwarven mine cart station. The mine",
-            "cart will take you to Keldagrim."
-        )
-        addDialogueAction(player) { _, _ ->
-            showKeldagrimEntryOptions(player)
-        }
-    }
-
-    private fun showKeldagrimEntryOptions(player: Player) {
-        sendDialogueOptions(player, "Select an option", "Travel to Keldagrim.", "Stay here.")
-        addDialogueAction(player) { _, option ->
-            if (option == 2) goToKeldagrim(player) else closeDialogue(player)
-        }
-    }
-
-    private fun showKeldagrimExitOptions(player: Player) {
-        val questDone = isQuestComplete(player, Quests.FISHING_CONTEST)
-        val options = buildList {
-            add("To the Grand Exchange.")
-            add("To Ice Mountain.")
-            if (questDone) add("To White Wolf Mountain.")
-            add("Stay here.")
-        }
-
-        sendDialogueOptions(player, "Select an Option", *options.toTypedArray())
-        addDialogueAction(player) { _, option ->
-            when (option) {
-                2 -> leaveKeldagrimTo(player, destinations.getValue("GE"))
-                3 -> leaveKeldagrimTo(player, destinations.getValue("Mine"))
-                4 -> if (questDone) leaveKeldagrimTo(player, destinations.getValue("Sindarpos")) else closeDialogue(player)
-                else -> closeDialogue(player)
-            }
-        }
     }
 
     companion object {
-
-        /**
-         * Starts the travel sequence to Keldagrim.
-         */
-        private fun goToKeldagrim(player: Player) {
+        private fun startTravelToKeldagrim(player: Player) {
             if (core.api.quest.hasRequirement(player, Quests.THE_GIANT_DWARF)) {
                 submitWorldPulse(TravelToKeldagrimPulse(player))
             }
         }
 
-        /**
-         * Starts the travel sequence from Keldagrim to a given destination.
-         */
-        private fun leaveKeldagrimTo(player: Player, dest: Location) {
+        private fun startTravelFromKeldagrim(player: Player, dest: Location) {
             if (core.api.quest.hasRequirement(player, Quests.THE_GIANT_DWARF)) {
                 submitWorldPulse(TravelFromKeldagrimPulse(player, dest))
             }
         }
 
-        /**
-         * Handles the pulse for traveling from Keldagrim.
-         */
         private class TravelFromKeldagrimPulse(
             val player: Player,
             val dest: Location
@@ -155,9 +124,6 @@ class KeldagrimMinecartListener : InteractionListener {
             }
         }
 
-        /**
-         * Handles the pulse for traveling to Keldagrim.
-         */
         private class TravelToKeldagrimPulse(val player: Player) : Pulse() {
             private var counter = 0
             private val cartNPC = NPC(NPCs.MINE_CART_1544)
@@ -170,7 +136,10 @@ class KeldagrimMinecartListener : InteractionListener {
                     8 -> player.moveCartTo(2914, 10173)
                     10 -> fadeToNormal()
                     23 -> finalizeTravel()
-                    33 -> cartNPC.clear().let { true }
+                    33 -> {
+                        cartNPC.clear()
+                        return true
+                    }
                 }
                 return false
             }
@@ -198,17 +167,11 @@ class KeldagrimMinecartListener : InteractionListener {
             }
         }
 
-        /**
-         * Teleports the player with cart appearance.
-         */
         private fun Player.teleportWithCart(location: Location, ride: Boolean) {
             properties.teleportLocation = location
             appearance.rideCart(ride)
         }
 
-        /**
-         * Moves the cart to a specified location.
-         */
         private fun Player.moveCartTo(x: Int, y: Int) {
             walkingQueue.reset()
             walkingQueue.addPath(x, y)
