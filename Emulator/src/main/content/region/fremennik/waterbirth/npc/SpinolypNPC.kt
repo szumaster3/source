d51fc71e88fed7e3290f48802c5d61b4a7b831ce
szumaster <szumaster2009@protonmail.com>
2025-06-25 20:04:14 +0200

    Small refactor of SpinolypNPC

--- Emulator/src/main/content/region/fremennik/waterbirth/npc/SpinolypNPC.kt
+++ Emulator/src/main/content/region/fremennik/waterbirth/npc/SpinolypNPC.kt
@@ -27,86 +27,69 @@ class SpinolypNPC(id: Int = 0, location: Location? = null) : AbstractNPC(id, loc
         getAggressiveHandler().isAllowTolerance = false
     }
 
-    override fun construct(id: Int, location: Location, vararg objects: Any): AbstractNPC = SpinolypNPC(id, location)
+    override fun construct(id: Int, location: Location, vararg objects: Any): AbstractNPC =
+        SpinolypNPC(id, location)
 
     override fun getSwingHandler(swing: Boolean): CombatSwingHandler = SWING_HANDLER
 
     override fun sendImpact(state: BattleState) {
-        if (state.estimatedHit == 0 && RandomFunction.random(15) < 2) {
-            state.estimatedHit = RandomFunction.random(1, 11)
-        }
-        if (state.estimatedHit > 11) {
-            state.estimatedHit = RandomFunction.random(3, 9)
+        state.estimatedHit = when {
+            state.estimatedHit == 0 && RandomFunction.random(15) < 2 -> RandomFunction.random(1, 11)
+            state.estimatedHit > 11 -> RandomFunction.random(3, 9)
+            else -> state.estimatedHit
         }
     }
 
     override fun getIds(): IntArray = intArrayOf(NPCs.SPINOLYP_2894, NPCs.SPINOLYP_2896)
 
+    companion object {
+        private val SPELL: CombatSpell? = SpellBookManager.SpellBook.MODERN.getSpell(14) as? CombatSpell
+        private val SWING_HANDLER = object : MultiSwingHandler(
+            SwitchAttack(CombatStyle.MAGIC.swingHandler, null),
+            SwitchAttack(CombatStyle.RANGE.swingHandler, null)
+        ) {
+            private var currentType: CombatStyle? = null
 
-    class SpinolpySwingHandler : MultiSwingHandler(
-        SwitchAttack(CombatStyle.MAGIC.swingHandler, null),
-        SwitchAttack(CombatStyle.RANGE.swingHandler, null)
-    ) {
-        private var currentType: CombatStyle? = null
-
-        override fun canSwing(entity: Entity, victim: Entity): InteractionType? {
-            var type = super.canSwing(entity, victim)
-            if (type == InteractionType.MOVE_INTERACT) {
-                type = InteractionType.NO_INTERACT
+            override fun canSwing(entity: Entity, victim: Entity): InteractionType {
+                val type = super.canSwing(entity, victim)
+                return if (type == InteractionType.MOVE_INTERACT) InteractionType.NO_INTERACT else type ?: InteractionType.NO_INTERACT
             }
-            return type
-        }
 
-        override fun swing(entity: Entity?, victim: Entity?, state: BattleState?): Int {
-            val swing = super.swing(entity, victim, state)
-            currentType = state?.style
-            if (currentType == CombatStyle.MAGIC) {
-                entity?.let { setSpell(it) }
+            override fun swing(entity: Entity?, victim: Entity?, state: BattleState?): Int {
+                val swing = super.swing(entity, victim, state)
+                currentType = state?.style
+                if (currentType == CombatStyle.MAGIC && entity != null) {
+                    entity.properties.spell = SPELL
+                    entity.properties.autocastSpell = SPELL
+                }
+                return swing
             }
-            return swing
-        }
 
-        override fun getCombatDistance(e: Entity, v: Entity, rawDistance: Int): Int = 12
+            override fun getCombatDistance(e: Entity, v: Entity, rawDistance: Int): Int = 12
 
-        override fun calculateDefence(victim: Entity?, attacker: Entity?): Int {
-            return when (currentType) {
-                CombatStyle.MAGIC -> CombatStyle.MAGIC.swingHandler.calculateDefence(victim, attacker)
-                CombatStyle.RANGE -> CombatStyle.RANGE.swingHandler.calculateDefence(victim, attacker)
-                else -> CombatStyle.RANGE.swingHandler.calculateDefence(victim, attacker)
+            override fun calculateDefence(victim: Entity?, attacker: Entity?): Int =
+                when (currentType) {
+                    CombatStyle.MAGIC -> CombatStyle.MAGIC.swingHandler.calculateDefence(victim, attacker)
+                    else -> CombatStyle.RANGE.swingHandler.calculateDefence(victim, attacker)
+                }
+
+            override fun visualize(entity: Entity, victim: Entity?, state: BattleState?) {
+                if (state?.style == CombatStyle.MAGIC) {
+                    entity.properties.spell = SPELL
+                    entity.properties.autocastSpell = SPELL
+                    state.spell = SPELL
+                }
+                state?.style?.swingHandler?.visualize(entity, victim, state)
+                entity.animator.forceAnimation(entity.properties.attackAnimation)
             }
-        }
 
-        override fun visualize(entity: Entity, victim: Entity?, state: BattleState?) {
-            super.visualize(entity, victim, state)
-            if (state?.style == CombatStyle.MAGIC) {
-                entity?.let { setSpell(it) }
-                val spell = SpellBookManager.SpellBook.MODERN.getSpell(14) as CombatSpell?
-                state.spell = spell
-            }
-            state?.style?.swingHandler?.visualize(entity, victim, state)
-            entity.animator.forceAnimation(entity.properties.attackAnimation)
-        }
-
-        override fun impact(entity: Entity?, victim: Entity?, state: BattleState?) {
-            super.impact(entity, victim, state)
-            if (currentType == CombatStyle.MAGIC && (state?.estimatedHit ?: 0) > 0) {
-                victim?.getSkills()?.decrementPrayerPoints(1.0)
-            } else {
-                if (RandomFunction.random(20) == 5) {
-                    if (victim != null && entity != null) {
-                        applyPoison(victim, entity, 30)
-                    }
+            override fun impact(entity: Entity?, victim: Entity?, state: BattleState?) {
+                if (state?.style == CombatStyle.MAGIC && (state.estimatedHit ?: 0) > 0) {
+                    victim?.getSkills()?.decrementPrayerPoints(1.0)
+                } else if (RandomFunction.random(20) == 5 && entity != null && victim != null) {
+                    applyPoison(victim, entity, 30)
                 }
             }
-        }
-    }
-
-    companion object {
-        private val SWING_HANDLER: CombatSwingHandler = SpinolpySwingHandler()
-        private fun setSpell(e: Entity) {
-            val spell = SpellBookManager.SpellBook.MODERN.getSpell(14) as CombatSpell?
-            e.properties.spell = spell
-            e.properties.autocastSpell = spell
         }
     }
 }
