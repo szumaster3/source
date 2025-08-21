de88ba649af128d54bc5c65df1de6871c41f6765
szumaster <szumaster2009@protonmail.com>
2025-08-21 02:59:23 +0200

    Fix removal dialogue

--- Server/src/main/content/global/skill/construction/RemovalDialogue.kt
+++ Server/src/main/content/global/skill/construction/RemovalDialogue.kt
@@ -9,78 +9,73 @@ import core.game.node.entity.player.Player
 import core.plugin.Initializable
 
 @Initializable
-class RemovalDialogue() : Dialogue() {
-
+class RemovalDialogue : Dialogue {
     private lateinit var pos: IntArray
+
     private var plane = 0
+
     private var room: Room? = null
 
-    constructor(player: Player?) : this() {
-        this.player = player
-    }
+    constructor() : super()
+
+    constructor(player: Player?) : super(player)
 
     override fun newInstance(player: Player?): Dialogue = RemovalDialogue(player)
 
     override fun open(vararg args: Any?): Boolean {
         pos = args[1] as IntArray
-        plane = if (HouseManager.isInDungeon(player)) 3 else player.location.z
+        plane = player.location.z
+        if (HouseManager.isInDungeon(player)) {
+            plane = 3
+        }
         room = player.houseManager.rooms[plane][pos[0]][pos[1]]
-
-        val roomName = room?.properties?.name
-            ?.lowercase()
-            ?.replace("_", " ")
-            ?: "room"
-
         setTitle(player, 2)
-        sendDialogueOptions(player, "Remove the $roomName?", "Yes", "No")
+        sendDialogueOptions(
+            player,
+            "Remove the " + (if (room != null) {
+                room!!.properties.name.lowercase().replace("_", " ")
+            } else {
+                "room"
+            }) + "?",
+            "Yes",
+            "No",
+        )
         stage = 0
         return true
     }
 
-    override fun handle(interfaceId: Int, buttonId: Int): Boolean {
-        if (stage == 0 && buttonId == 1) {
-            val manager = player.houseManager
-
-            /*
-             * Do not remove a room if it supports another.
-             */
-
-            if (plane == 0 && manager.hasRoomAt(1, pos[0], pos[1])) {
-                sendPlainDialogue(player, false, "You can't remove a room supporting another room.")
-                stage = 1
-                return true
-            }
-
-            /*
-             * Do not remove the garden with the portal.
-             */
-
-            if (room?.properties?.isLand == true) {
-                val h = room?.hotspots?.getOrNull(0)
-                if (h?.decorationIndex == 0 && manager.portalAmount <= 1) {
-                    sendPlainDialogue(player, false, "You can't remove the garden with your portal in it.")
+    override fun handle(
+        interfaceId: Int,
+        buttonId: Int,
+    ): Boolean {
+        if (stage == 0) {
+            if (buttonId == 1) {
+                if (plane == 0 && player.houseManager.hasRoomAt(1, pos[0], pos[1])) {
+                    sendPlainDialogue(player, false, "You can't remove a room supporting another room.")
                     stage = 1
                     return true
                 }
-            }
-
-            /*
-             * Removal of a room and roof.
-             */
-
-            manager.rooms[plane][pos[0]][pos[1]] = null
-            for (i in plane..2) {
-                if (manager.rooms[i][pos[0]][pos[1]]?.properties?.isRoof == true) {
-                    manager.rooms[i][pos[0]][pos[1]] = null
+                if (room != null && room!!.properties.isLand) {
+                    val h = room!!.hotspots[0]
+                    if (h != null && h.decorationIndex == 0 && player.houseManager.portalAmount <= 1) {
+                        sendPlainDialogue(player, false, "You can't remove the garden with your portal in it.")
+                        stage = 1
+                        return true
+                    }
                 }
+                player.houseManager.rooms[plane][pos[0]][pos[1]] = null
+                for (i in plane..2) {
+                    val r = player.houseManager.rooms[i][pos[0]][pos[1]]
+                    if (r != null && r.properties.isRoof) {
+                        player.houseManager.rooms[i][pos[0]][pos[1]] = null
+                    }
+                }
+                player.houseManager.reload(player, true)
             }
-            manager.reload(player, true)
         }
-
         end()
         return true
     }
 
-    override fun getIds(): IntArray =
-        intArrayOf(DialogueInterpreter.getDialogueKey("con:remove"))
+    override fun getIds(): IntArray = intArrayOf(DialogueInterpreter.getDialogueKey("con:remove"))
 }
