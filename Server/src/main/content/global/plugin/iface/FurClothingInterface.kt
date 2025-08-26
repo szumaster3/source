1e8cfacbd1e76b5303fde82351e50c242e692578
szumaster <szumaster2009@protonmail.com>
2025-08-27 01:06:46 +0200

    Convert fur clothing interface to listener

--- Server/src/main/content/global/plugin/iface/FurClothingInterface.kt
+++ Server/src/main/content/global/plugin/iface/FurClothingInterface.kt
@@ -1,17 +1,11 @@
 package content.global.plugin.iface
 
 import core.api.*
-import core.game.component.Component
-import core.game.component.ComponentDefinition
-import core.game.component.ComponentPlugin
 import core.game.container.access.InterfaceContainer
+import core.game.interaction.InterfaceListener
 import core.game.node.entity.player.Player
 import core.game.node.item.Item
-import core.plugin.Initializable
-import core.plugin.Plugin
 import core.tools.colorize
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import shared.consts.Components
 import shared.consts.Items
 
@@ -44,109 +38,79 @@ private val SPOTTED_CAPE = Item(Items.SPOTTED_CAPE_10069)
 private val SPOTTIER_CAPE = Item(Items.SPOTTIER_CAPE_10071)
 
 private val OPTIONS = arrayOf("Buy 10", "Buy 5", "Buy 1", "Value")
-
 private const val FUR_CLOTHING_COMPONENT_ID = Components.CUSTOM_FUR_CLOTHING_477
-private val ITEMS = arrayOf(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, POLAR_CAMO_TOP, null, null, POLAR_CAMO_LEGS, null, null, null, null, null, WOOD_CAMO_TOP, null, null, WOOD_CAMO_LEGS, null, null, null, null, JUNGLE_CAMO_TOP, null, null, JUNGLE_CAMO_LEGS, null, null, null, null, DESERT_CAMO_TOP, null, null, DESERT_CAMO_LEGS, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, LARUPIA_HAT, null, null, LARUPIA_TOP, null, null, LARUPIA_LEGS, null, null, null, null, GRAAHK_HEADDRESS, null, null, GRAAHK_TOP, null, null, GRAAHK_LEGS, null, null, null, null, null, KYATT_HAT, null, null, KYATT_TOP, null, null, KYATT_LEGS, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, GLOVES_OF_SILENCE, null, null, null, null, null, null, null, null, null, null, SPOTTED_CAPE, null, null, null, null, null, null, null, null, null, null, null, SPOTTIER_CAPE, null)
+
+private val ITEMS = arrayOf(
+    POLAR_CAMO_TOP, POLAR_CAMO_LEGS, null, WOOD_CAMO_TOP, WOOD_CAMO_LEGS, null, JUNGLE_CAMO_TOP, JUNGLE_CAMO_LEGS, null, DESERT_CAMO_TOP, DESERT_CAMO_LEGS,
+    LARUPIA_HAT, LARUPIA_TOP, LARUPIA_LEGS, null, GRAAHK_HEADDRESS, GRAAHK_TOP, GRAAHK_LEGS, null, KYATT_HAT, KYATT_TOP, KYATT_LEGS,
+    null, GLOVES_OF_SILENCE, null, null, null, SPOTTED_CAPE, null, null, null, SPOTTIER_CAPE
+)
 
 /**
- * Fur Clothing interface.
- *
- * @author Ceikry
+ * Handles custom fur clothing interface.
  */
-@Initializable
-class FurClothingInterface : ComponentPlugin() {
+class FurClothingInterface : InterfaceListener {
 
-    override fun open(player: Player?, component: Component?) {
-        super.open(player, component)
-        player ?: return
-        val nonNullItems = ITEMS.filterNotNull().toTypedArray()
-        InterfaceContainer.generateItems(player, nonNullItems, OPTIONS, FUR_CLOTHING_COMPONENT_ID, 26, 12, 33, 5000)
-        GlobalScope.launch {
+    override fun defineInterfaceListeners() {
+        onOpen(FUR_CLOTHING_COMPONENT_ID) { player, _ ->
+            InterfaceContainer.generateItems(player, ITEMS, OPTIONS, FUR_CLOTHING_COMPONENT_ID, 26, 3, 11, 5000)
             val checkedFurs = arrayListOf<String>()
-            for (clothing in FUR_CLOTHING.values()) {
+            for (clothing in FurClothing.values()) {
                 if (checkedFurs.contains(clothing.textContent)) continue
-                if (inInventory(player, clothing.requiredFur.id, clothing.requiredFur.amount)) {
+                if (inInventory(player, clothing.type.id, clothing.type.amount)) {
                     sendString(
                         player,
                         colorize("%G${clothing.textContent}"),
                         FUR_CLOTHING_COMPONENT_ID,
-                        clothing.textChildID,
+                        clothing.textChildID
                     )
                     checkedFurs.add(clothing.textContent)
                 }
             }
+            return@onOpen true
+        }
+
+        on(FUR_CLOTHING_COMPONENT_ID) { player, _, opcode, _, slotID, _ ->
+            val itemMap = mapOf(
+                0  to FurClothing.POLAR_KEBBIT_TOP,
+                1  to FurClothing.POLAR_KEBBIT_BOT,
+                3  to FurClothing.COMMON_KEBBIT_TOP,
+                4  to FurClothing.COMMON_KEBBIT_BOT,
+                6  to FurClothing.FELDIP_WEASEL_TOP,
+                7  to FurClothing.FELDIP_WEASEL_LEGS,
+                9  to FurClothing.DESERT_DEVIL_TOP,
+                10 to FurClothing.DESERT_DEVIL_LEGS,
+                11 to FurClothing.LARUPIA_HEAD,
+                12 to FurClothing.LARUPIA_CHEST,
+                13 to FurClothing.LARUPIA_BOTTOM,
+                15 to FurClothing.GRAAHK_HEAD,
+                16 to FurClothing.GRAAHK_CHEST,
+                17 to FurClothing.GRAAHK_BOTTOM,
+                19 to FurClothing.KYATT_HEAD,
+                20 to FurClothing.KYATT_CHEST,
+                21 to FurClothing.KYATT_BOTTOM,
+                23 to FurClothing.GLOVES_SILENCE,
+                27 to FurClothing.SPOT_CAPE,
+                31 to FurClothing.DASH_CAPE
+            )
+            val clothing = itemMap[slotID] ?: return@on true
+            when (opcode) {
+                196 -> buy(player, clothing, 1)
+                124 -> buy(player, clothing, 5)
+                199 -> buy(player, clothing, 10)
+                155 -> sendMessage(
+                    player,
+                    "${clothing.product.name} requires ${clothing.type.amount} ${clothing.type.name.lowercase()} and costs ${clothing.price} coins."
+                )
+                9 -> sendMessage(player, itemDefinition(clothing.product.id).examine)
+            }
+            return@on true
         }
     }
 
-    override fun handle(
-        player: Player?,
-        component: Component?,
-        opcode: Int,
-        button: Int,
-        slot: Int,
-        itemId: Int,
-    ): Boolean {
-        player ?: return false
-
-        val clothingItem: FUR_CLOTHING? = when (slot) {
-            34 -> FUR_CLOTHING.POLAR_KEBBIT_TOP
-            37 -> FUR_CLOTHING.POLAR_KEBBIT_BOT
-            43 -> FUR_CLOTHING.COMMON_KEBBIT_TOP
-            46 -> FUR_CLOTHING.COMMON_KEBBIT_BOT
-            51 -> FUR_CLOTHING.FELDIP_WEASEL_TOP
-            54 -> FUR_CLOTHING.FELDIP_WEASEL_LEGS
-            59 -> FUR_CLOTHING.DESERT_DEVIL_TOP
-            62 -> FUR_CLOTHING.DESERT_DEVIL_LEGS
-            166 -> FUR_CLOTHING.LARUPIA_HEAD
-            169 -> FUR_CLOTHING.LARUPIA_CHEST
-            172 -> FUR_CLOTHING.LARUPIA_BOT
-            177 -> FUR_CLOTHING.GRAAHK_HEAD
-            180 -> FUR_CLOTHING.GRAAHK_CHEST
-            183 -> FUR_CLOTHING.GRAAHK_BOT
-            189 -> FUR_CLOTHING.KYATT_HEAD
-            192 -> FUR_CLOTHING.KYATT_CHEST
-            195 -> FUR_CLOTHING.KYATT_BOT
-            334 -> FUR_CLOTHING.GLOVES_SILENCE
-            345 -> FUR_CLOTHING.SPOT_CAPE
-            357 -> FUR_CLOTHING.DASH_CAPE
-            else -> null
-        }
-
-        when (opcode) {
-            196 -> clothingItem?.let { buy(player, it, 1) }
-            124 -> clothingItem?.let { buy(player, it, 5) }
-            199 -> clothingItem?.let { buy(player, it, 10) }
-            155 -> clothingItem?.let { value(player, it) }
-            9 -> clothingItem?.let { examine(player, it) }
-        }
-        return true
-    }
-
-    private fun value(
-        player: Player,
-        clothing: FUR_CLOTHING,
-    ) {
-        sendMessage(
-            player,
-            "${clothing.product.name} requires ${clothing.requiredFur.amount} ${clothing.requiredFur.name.lowercase()} and costs ${clothing.price} coins.",
-        )
-    }
-
-    private fun examine(
-        player: Player,
-        clothing: FUR_CLOTHING,
-    ) {
-        sendMessage(player, itemDefinition(clothing.product.id).examine)
-    }
-
-    private fun buy(
-        player: Player,
-        clothing: FUR_CLOTHING,
-        amount: Int,
-    ) {
-        val coins = Item(Items.COINS_995, clothing.price * amount)
-        val amtFurRequired = clothing.requiredFur.amount * amount
-        val requiredFur = Item(clothing.requiredFur.id, amtFurRequired)
+    private fun buy(player: Player, fur: FurClothing, amount: Int) {
+        val coins = Item(Items.COINS_995, fur.price * amount)
+        val requiredFur = Item(fur.type.id, fur.type.amount * amount)
 
         if (!inInventory(player, requiredFur.id, requiredFur.amount)) {
             sendDialogue(player, "You don't have enough fur for that.")
@@ -160,160 +124,32 @@ class FurClothingInterface : ComponentPlugin() {
 
         removeItem(player, requiredFur, Container.INVENTORY)
         removeItem(player, coins, Container.INVENTORY)
-        addItem(player, clothing.product.id, amount)
+        addItem(player, fur.product.id, amount)
     }
 
-    override fun newInstance(arg: Any?): Plugin<Any> {
-        ComponentDefinition.put(FUR_CLOTHING_COMPONENT_ID, this)
-        return this
-    }
-
-    internal enum class FUR_CLOTHING(
-        val requiredFur: Item,
-        val price: Int,
-        val product: Item,
-        val textChildID: Int,
-        val textContent: String,
-    ) {
-        POLAR_KEBBIT_TOP(
-            requiredFur = Item(Items.POLAR_KEBBIT_FUR_10117, 2),
-            price = 20,
-            product = POLAR_CAMO_TOP,
-            textChildID = 18,
-            textContent = "Polar kebbit",
-        ),
-        POLAR_KEBBIT_BOT(
-            requiredFur = Item(Items.POLAR_KEBBIT_FUR_10117, 2),
-            price = 20,
-            product = POLAR_CAMO_LEGS,
-            textChildID = 18,
-            textContent = "Polar kebbit",
-        ),
-        COMMON_KEBBIT_TOP(
-            requiredFur = Item(Items.COMMON_KEBBIT_FUR_10121, 2),
-            price = 20,
-            product = WOOD_CAMO_TOP,
-            textChildID = 19,
-            textContent = "Common kebbit",
-        ),
-        COMMON_KEBBIT_BOT(
-            requiredFur = Item(Items.COMMON_KEBBIT_FUR_10121, 2),
-            price = 20,
-            product = WOOD_CAMO_LEGS,
-            textChildID = 19,
-            textContent = "Common kebbit",
-        ),
-        FELDIP_WEASEL_TOP(
-            requiredFur = Item(Items.FELDIP_WEASEL_FUR_10119, 2),
-            price = 20,
-            product = JUNGLE_CAMO_TOP,
-            textChildID = 20,
-            textContent = "Feldip weasel",
-        ),
-        FELDIP_WEASEL_LEGS(
-            requiredFur = Item(Items.FELDIP_WEASEL_FUR_10119, 2),
-            price = 20,
-            product = JUNGLE_CAMO_LEGS,
-            textChildID = 20,
-            textContent = "Feldip weasel",
-        ),
-        DESERT_DEVIL_TOP(
-            requiredFur = Item(Items.DESERT_DEVIL_FUR_10123, 2),
-            price = 20,
-            product = DESERT_CAMO_TOP,
-            textChildID = 21,
-            textContent = "Desert devil",
-        ),
-        DESERT_DEVIL_LEGS(
-            requiredFur = Item(Items.DESERT_DEVIL_FUR_10123, 2),
-            price = 20,
-            product = DESERT_CAMO_LEGS,
-            textChildID = 21,
-            textContent = "Desert devil",
-        ),
-        LARUPIA_HEAD(
-            requiredFur = Item(Items.LARUPIA_FUR_10095),
-            price = 500,
-            product = LARUPIA_HAT,
-            textChildID = 15,
-            textContent = "Larupia",
-        ),
-        LARUPIA_CHEST(
-            requiredFur = Item(Items.LARUPIA_FUR_10095),
-            price = 100,
-            product = LARUPIA_TOP,
-            textChildID = 15,
-            textContent = "Larupia",
-        ),
-        LARUPIA_BOT(
-            requiredFur = Item(Items.LARUPIA_FUR_10095),
-            price = 100,
-            product = LARUPIA_LEGS,
-            textChildID = 15,
-            textContent = "Larupia",
-        ),
-        GRAAHK_HEAD(
-            requiredFur = Item(Items.GRAAHK_FUR_10099),
-            price = 750,
-            product = GRAAHK_HEADDRESS,
-            textChildID = 16,
-            textContent = "Graahk",
-        ),
-        GRAAHK_CHEST(
-            requiredFur = Item(Items.GRAAHK_FUR_10099),
-            price = 150,
-            product = GRAAHK_TOP,
-            textChildID = 16,
-            textContent = "Graahk",
-        ),
-        GRAAHK_BOT(
-            requiredFur = Item(Items.GRAAHK_FUR_10099),
-            price = 150,
-            product = GRAAHK_LEGS,
-            textChildID = 16,
-            textContent = "Graahk",
-        ),
-        KYATT_HEAD(
-            requiredFur = Item(Items.KYATT_FUR_10103),
-            price = 1000,
-            product = KYATT_HAT,
-            textChildID = 17,
-            textContent = "Kyatt",
-        ),
-        KYATT_CHEST(
-            requiredFur = Item(Items.KYATT_FUR_10103),
-            price = 200,
-            product = KYATT_TOP,
-            textChildID = 17,
-            textContent = "Kyatt",
-        ),
-        KYATT_BOT(
-            requiredFur = Item(Items.KYATT_FUR_10103),
-            price = 200,
-            product = KYATT_LEGS,
-            textChildID = 17,
-            textContent = "Kyatt",
-        ),
-        GLOVES_SILENCE(
-            requiredFur = Item(Items.DARK_KEBBIT_FUR_10115, 2),
-            price = 600,
-            product = GLOVES_OF_SILENCE,
-            textChildID = 22,
-            textContent = "Dark kebbit",
-        ),
-        SPOT_CAPE(
-            requiredFur = Item(Items.SPOTTED_KEBBIT_FUR_10125, 2),
-            price = 400,
-            product = SPOTTED_CAPE,
-            textChildID = 23,
-            textContent = "Spotted kebbit",
-        ),
-        DASH_CAPE(
-            requiredFur = Item(Items.DASHING_KEBBIT_FUR_10127, 2),
-            price = 800,
-            product = SPOTTIER_CAPE,
-            textChildID = 24,
-            textContent = "Dashing kebbit",
-        ),
+    /**
+     * Represents the custom fur clothing data.
+     */
+    internal enum class FurClothing(val type: Item, val price: Int, val product: Item, val textChildID: Int, val textContent: String) {
+        POLAR_KEBBIT_TOP(Item(Items.POLAR_KEBBIT_FUR_10117, 2), 20, POLAR_CAMO_TOP, 18, "Polar kebbit"),
+        POLAR_KEBBIT_BOT(Item(Items.POLAR_KEBBIT_FUR_10117, 2), 20, POLAR_CAMO_LEGS, 18, "Polar kebbit"),
+        COMMON_KEBBIT_TOP(Item(Items.COMMON_KEBBIT_FUR_10121, 2), 20, WOOD_CAMO_TOP, 19, "Common kebbit"),
+        COMMON_KEBBIT_BOT(Item(Items.COMMON_KEBBIT_FUR_10121, 2), 20, WOOD_CAMO_LEGS, 19, "Common kebbit"),
+        FELDIP_WEASEL_TOP(Item(Items.FELDIP_WEASEL_FUR_10119, 2), 20, JUNGLE_CAMO_TOP, 20, "Feldip weasel"),
+        FELDIP_WEASEL_LEGS(Item(Items.FELDIP_WEASEL_FUR_10119, 2), 20, JUNGLE_CAMO_LEGS, 20, "Feldip weasel"),
+        DESERT_DEVIL_TOP(Item(Items.DESERT_DEVIL_FUR_10123, 2), 20, DESERT_CAMO_TOP, 21, "Desert devil"),
+        DESERT_DEVIL_LEGS(Item(Items.DESERT_DEVIL_FUR_10123, 2), 20, DESERT_CAMO_LEGS, 21, "Desert devil"),
+        LARUPIA_HEAD(Item(Items.LARUPIA_FUR_10095), 500, LARUPIA_HAT, 15, "Larupia"),
+        LARUPIA_CHEST(Item(Items.LARUPIA_FUR_10095), 100, LARUPIA_TOP, 15, "Larupia"),
+        LARUPIA_BOTTOM(Item(Items.LARUPIA_FUR_10095), 100, LARUPIA_LEGS, 15, "Larupia"),
+        GRAAHK_HEAD(Item(Items.GRAAHK_FUR_10099), 750, GRAAHK_HEADDRESS, 16, "Graahk"),
+        GRAAHK_CHEST(Item(Items.GRAAHK_FUR_10099), 150, GRAAHK_TOP, 16, "Graahk"),
+        GRAAHK_BOTTOM(Item(Items.GRAAHK_FUR_10099), 150, GRAAHK_LEGS, 16, "Graahk"),
+        KYATT_HEAD(Item(Items.KYATT_FUR_10103), 1000, KYATT_HAT, 17, "Kyatt"),
+        KYATT_CHEST(Item(Items.KYATT_FUR_10103), 200, KYATT_TOP, 17, "Kyatt"),
+        KYATT_BOTTOM(Item(Items.KYATT_FUR_10103), 200, KYATT_LEGS, 17, "Kyatt"),
+        GLOVES_SILENCE(Item(Items.DARK_KEBBIT_FUR_10115, 2), 600, GLOVES_OF_SILENCE, 22, "Dark kebbit"),
+        SPOT_CAPE(Item(Items.SPOTTED_KEBBIT_FUR_10125, 2), 400, SPOTTED_CAPE, 23, "Spotted kebbit"),
+        DASH_CAPE(Item(Items.DASHING_KEBBIT_FUR_10127, 2), 800, SPOTTIER_CAPE, 24, "Dashing kebbit")
     }
 }
