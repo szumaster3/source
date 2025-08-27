1eded612a92d136d32493d49e17afdf27cc9badc
szumaster <szumaster2009@protonmail.com>
2025-08-28 01:03:54 +0200

    Fix barrage projectile

--- Server/src/main/content/global/skill/magic/spells/ancient/IceSpellDefinition.kt
+++ Server/src/main/content/global/skill/magic/spells/ancient/IceSpellDefinition.kt
@@ -2,6 +2,7 @@ package content.global.skill.magic.spells.ancient
 
 import content.global.skill.magic.spells.AncientSpells
 import content.global.skill.magic.spells.SpellProjectile
+import core.game.node.entity.Entity
 import core.game.node.entity.combat.spell.Runes
 import core.game.node.entity.combat.spell.SpellType
 import core.game.node.entity.impl.Animator
@@ -18,5 +19,5 @@ enum class IceSpellDefinition(val type: SpellType, val button: Int, val level: I
     RUSH(SpellType.RUSH, AncientSpells.ICE_RUSH, 58, 34.0, Sounds.ICE_RUSH_IMPACT_173, Animation(1978, Animator.Priority.HIGH), null, SpellProjectile.create(360), Graphics(361, 96), arrayOf(Runes.DEATH_RUNE.getItem(2), Runes.CHAOS_RUNE.getItem(2), Runes.WATER_RUNE.getItem(2))),
     BURST(SpellType.BURST, AncientSpells.ICE_BURST, 70, 40.0, Sounds.ICE_BURST_IMPACT_170, Animation(1979, Animator.Priority.HIGH), null, SpellProjectile.create(362), Graphics(363, 0), arrayOf(Runes.DEATH_RUNE.getItem(2), Runes.CHAOS_RUNE.getItem(4), Runes.WATER_RUNE.getItem(4))),
     BLITZ(SpellType.BLITZ, AncientSpells.ICE_BLITZ, 82, 46.0, Sounds.ICE_BLITZ_IMPACT_169, Animation(1978, Animator.Priority.HIGH), Graphics(366, 96), null, Graphics(367, 96), arrayOf(Runes.BLOOD_RUNE.getItem(2), Runes.DEATH_RUNE.getItem(2), Runes.WATER_RUNE.getItem(3))),
-    BARRAGE(SpellType.BARRAGE, AncientSpells.ICE_BARRAGE, 94, 52.0, Sounds.ICE_BARRAGE_IMPACT_168, Animation(1979, Animator.Priority.HIGH), null, SpellProjectile.create(368), Graphics(369, 0), arrayOf(Runes.BLOOD_RUNE.getItem(2), Runes.DEATH_RUNE.getItem(4), Runes.WATER_RUNE.getItem(6)));
+    BARRAGE(SpellType.BARRAGE, AncientSpells.ICE_BARRAGE, 94, 52.0, Sounds.ICE_BARRAGE_IMPACT_168, Animation(1979, Animator.Priority.HIGH), null, Projectile.create(null as Entity?, null as Entity?, 368, 0, 0, 52, 75, 15, 11), Graphics(369, 0), arrayOf(Runes.BLOOD_RUNE.getItem(2), Runes.DEATH_RUNE.getItem(4), Runes.WATER_RUNE.getItem(6)));
 }
