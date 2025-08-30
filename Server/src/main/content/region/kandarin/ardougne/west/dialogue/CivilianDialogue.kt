77d56f7b466121881aca22cb8c15fbdfa219f250
szumaster <szumaster2009@protonmail.com>
2025-08-30 04:55:56 +0200

    Improve civilian interaction and dialogue

--- Server/src/main/content/region/kandarin/ardougne/west/dialogue/CivilianDialogue.kt
+++ Server/src/main/content/region/kandarin/ardougne/west/dialogue/CivilianDialogue.kt
@@ -1,6 +1,7 @@
 package content.region.kandarin.ardougne.west.dialogue
 
-import content.region.misthalin.draynor.quest.swept_cat.grownCatItemIds
+import content.global.skill.construction.decoration.costumeroom.Storable
+import content.global.skill.construction.decoration.costumeroom.Storable.Companion.hasStorableEquipped
 import core.api.*
 import core.game.dialogue.Dialogue
 import core.game.dialogue.FaceAnim
@@ -11,95 +12,121 @@ import core.tools.END_DIALOGUE
 import shared.consts.Items
 import shared.consts.NPCs
 
-/**
- * Represents the Civilian dialogue.
- */
+val cats = intArrayOf(
+    Items.PET_CAT_1561,
+    Items.PET_CAT_1562,
+    Items.PET_CAT_1563,
+    Items.PET_CAT_1564,
+    Items.PET_CAT_1565,
+    Items.PET_CAT_1566,
+    Items.OVERGROWN_CAT_1567,
+    Items.OVERGROWN_CAT_1568,
+    Items.OVERGROWN_CAT_1569,
+    Items.OVERGROWN_CAT_1570,
+    Items.OVERGROWN_CAT_1571,
+    Items.OVERGROWN_CAT_1572,
+    Items.LAZY_CAT_6549,
+    Items.LAZY_CAT_6550,
+    Items.LAZY_CAT_6551,
+    Items.LAZY_CAT_6552,
+    Items.LAZY_CAT_6553,
+    Items.LAZY_CAT_6554,
+    Items.WILY_CAT_6555,
+    Items.WILY_CAT_6556,
+    Items.WILY_CAT_6557,
+    Items.WILY_CAT_6558,
+    Items.WILY_CAT_6559,
+    Items.WILY_CAT_6560
+)
+
 @Initializable
 class CivilianDialogue(player: Player? = null) : Dialogue(player) {
 
     override fun open(vararg args: Any?): Boolean {
         npc = args[0] as NPC
-        if (allInEquipment(player, *hasMournerSet)) {
+        if(!hasStorableEquipped(player, Storable.MournerGear)) {
             player("Hello.")
             stage = 20
-            return true
         }
         when (npc.id) {
+            NPCs.CIVILIAN_785 -> playerl(FaceAnim.FRIENDLY, "Hello there.")
             NPCs.CIVILIAN_786 -> player("Hi there.")
-            else -> playerl(FaceAnim.FRIENDLY, "Hello there.").also { stage = 15 }
+            NPCs.CIVILIAN_787 -> playerl(FaceAnim.FRIENDLY, "Hello there.")
+            else -> return false
         }
+        stage = 0
         return true
     }
 
     override fun handle(interfaceId: Int, buttonId: Int): Boolean {
         when (stage) {
-            0 -> npcl(FaceAnim.FRIENDLY, "Good day to you, traveller.").also { stage++ }
-            1 -> playerl(FaceAnim.FRIENDLY, "What are you up to?").also { stage++ }
-            2 -> npcl(FaceAnim.FRIENDLY, "Chasing mice as usual! It's all I seem to do nowadays.").also { stage++ }
-            3 -> playerl(FaceAnim.FRIENDLY, "You must waste a lot of time?").also { stage++ }
-            4 -> npcl(FaceAnim.FRIENDLY, "Yes, but what can you do? It's not like there's many cats around here!").also { stage++ }
-            5 -> {
+            0 -> {
+                when (npc.id) {
+                    NPCs.CIVILIAN_785 -> npcl(FaceAnim.FRIENDLY, "Oh hello, I'm sorry, I'm a bit worn out.")
+                    NPCs.CIVILIAN_786 -> npcl(FaceAnim.FRIENDLY, "Good day to you traveller.")
+                    NPCs.CIVILIAN_787 -> npcl(FaceAnim.FRIENDLY, "I'm a bit busy to talk right now, sorry.")
+                }
+                stage++
+            }
+            1 -> {
+                when (npc.id) {
+                    NPCs.CIVILIAN_785 -> playerl(FaceAnim.FRIENDLY, "Busy day?")
+                    NPCs.CIVILIAN_786 -> playerl(FaceAnim.FRIENDLY, "What are you up to?")
+                    NPCs.CIVILIAN_787 -> playerl(FaceAnim.FRIENDLY, "Why? What are you doing?")
+                }
+                stage++
+            }
+            2 -> {
+                when (npc.id) {
+                    NPCs.CIVILIAN_785 -> npcl(FaceAnim.FRIENDLY, "Oh, It's those mice! They're everywhere! What I really need is a cat. But they're hard to come by nowadays.")
+                    NPCs.CIVILIAN_786 -> npcl(FaceAnim.FRIENDLY, "Chasing mice as usual! It's all I seem to do nowadays.")
+                    NPCs.CIVILIAN_787 -> npcl(FaceAnim.FRIENDLY, "Trying to kill these mice! What I really need is a cat!")
+                }
+                stage++
+            }
+            3 -> {
+                val hasCat = cats.any { inInventory(player, it) }
                 when {
-                    anyInInventory(player, *hasKitten) -> {
-                        playerl(FaceAnim.HAPPY, "I have a kitten that I could sell.")
-                        stage++
-                    }
-
-                    anyInInventory(player, *hasCat) -> {
+                    hasCat -> {
                         playerl(FaceAnim.HAPPY, "I have a cat that I could sell.")
-                        stage = 9
+                        stage = 10
                     }
-
                     inInventory(player, Items.WITCHS_CAT_1491) -> {
                         player(FaceAnim.FRIENDLY, "I have a cat...look!")
-                        stage = 21
+                        stage = 20
                     }
-
                     else -> {
                         playerl(FaceAnim.NEUTRAL, "No, you're right, you don't see many around.")
                         stage = END_DIALOGUE
                     }
                 }
             }
-
-            6 -> npcl(FaceAnim.HAPPY, "Really, let's have a look.").also { stage++ }
-            7 -> sendDialogue(player, "You present the kitten.").also { stage++ }
-            8 -> npcl(FaceAnim.LAUGH, "Hah! that little thing won't catch any mice. I need a fully grown cat.").also { stage = END_DIALOGUE }
-            9 -> npcl(FaceAnim.FRIENDLY, "You don't say. Is that it?").also { stage++ }
-            10 -> player("Say hello to a real mouse-killer!").also { stage++ }
-            11 -> npcl(FaceAnim.FRIENDLY, "Hmmm, not bad, not bad at all. Looks like it's a lively one.").also { stage++ }
-            12 -> playerl(FaceAnim.THINKING, "Erm...kind of...").also { stage++ }
-            13 -> npcl(FaceAnim.FRIENDLY, "I don't have much in the way of money, but I do have these!").also { stage++ }
-            14 -> {
-                end()
-                val item = grownCatItemIds.indices
-                for (i in item) {
-                    if (removeItem(player, grownCatItemIds[i])) {
-                        player.familiarManager.removeDetails(i)
-                        sendItemDialogue(player, Items.DEATH_RUNE_560, "The peasant shows you a sack of death runes.")
+            10 -> npcl(FaceAnim.FRIENDLY, "You don't say, is that it?").also { stage++ }
+            11 -> player("Say hello to a real mouse-killer!").also { stage++ }
+            12 -> npcl(FaceAnim.FRIENDLY, "Hmmm, not bad, not bad at all. Looks like it's a lively one.").also { stage++ }
+            13 -> playerl(FaceAnim.THINKING, "Erm...kind of...").also { stage++ }
+            14 -> npcl(FaceAnim.FRIENDLY, "I don't have much in the way of money, but I do have these!").also { stage++ }
+            15 -> {
+                cats.forEachIndexed { index, catItem ->
+                    if (removeItem(player, catItem)) {
+                        player.familiarManager.removeDetails(index)
                         addItem(player, Items.DEATH_RUNE_560, 100)
+                        sendItemDialogue(player, Items.DEATH_RUNE_560, "The peasant shows you a sack of death runes.")
+                        stage = END_DIALOGUE
+                        return true
                     }
                 }
+                stage = END_DIALOGUE
             }
-
-            15 -> npcl(FaceAnim.FRIENDLY, "I'm a bit busy to talk right now, sorry.").also { stage++ }
-            16 -> playerl(FaceAnim.FRIENDLY, "Why? What are you doing?").also { stage++ }
-            17 -> npcl(FaceAnim.FRIENDLY, "Trying to kill these mice! What I really need is a cat!").also { stage++ }
-            18 -> playerl(FaceAnim.FRIENDLY, "No, you're right, you don't see many around.").also { stage = END_DIALOGUE }
-            19 -> npcl(FaceAnim.FRIENDLY, "If you Mourners really wanna help, why don't you do something about these mice?!").also { stage = END_DIALOGUE }
-            20 -> npcl(FaceAnim.HALF_ASKING, "If you Mourners really wanna help, why don't you do something about these mice?!").also { stage = END_DIALOGUE }
-            21 -> npcl(FaceAnim.NEUTRAL, "Hmmm...doesn't look like it's seen daylight in years. That's not going to catch any mice!").also { stage = END_DIALOGUE }
+            20 -> npcl(FaceAnim.NEUTRAL, "Hmmm...doesn't look like it's seen daylight in years. That's not going to catch any mice!").also { stage = END_DIALOGUE }
+            21 -> npcl(FaceAnim.HALF_ASKING, "If you Mourners really wanna help, why don't you do something about these mice?!").also { stage = END_DIALOGUE }
         }
         return true
     }
 
-    override fun getIds(): IntArray = intArrayOf(NPCs.CIVILIAN_785, NPCs.CIVILIAN_786, NPCs.CIVILIAN_787)
-
-    companion object {
-        private val hasMournerSet = intArrayOf(Items.GAS_MASK_1506, Items.MOURNER_TOP_6065, Items.MOURNER_TROUSERS_6067, Items.MOURNER_BOOTS_6069, Items.MOURNER_CLOAK_6070, Items.MOURNER_GLOVES_6068)
-        private val hasKitten = intArrayOf(NPCs.CLOCKWORK_CAT_3598, NPCs.KITTEN_8217, NPCs.HELL_KITTEN_3505, NPCs.KITTEN_766, NPCs.KITTEN_765, NPCs.KITTEN_764, NPCs.KITTEN_763, NPCs.KITTEN_762, NPCs.KITTEN_761)
-        private val hasCat = intArrayOf(Items.PET_CAT_1561, Items.PET_CAT_1562, Items.PET_CAT_1563, Items.PET_CAT_1564, Items.PET_CAT_1565, Items.PET_CAT_1566, Items.OVERGROWN_CAT_1567, Items.OVERGROWN_CAT_1568, Items.OVERGROWN_CAT_1569, Items.OVERGROWN_CAT_1570, Items.OVERGROWN_CAT_1571, Items.OVERGROWN_CAT_1572, Items.LAZY_CAT_6551, Items.LAZY_CAT_6552, Items.LAZY_CAT_6553, Items.LAZY_CAT_6554, Items.WILY_CAT_6555, Items.WILY_CAT_6556, Items.WILY_CAT_6557, Items.WILY_CAT_6558, Items.WILY_CAT_6559, Items.WILY_CAT_6560, Items.HELL_CAT_7582, Items.OVERGROWN_HELLCAT_7581, Items.LAZY_HELL_CAT_7584, Items.WILY_HELLCAT_7585)
-    }
+    override fun getIds(): IntArray = intArrayOf(
+        NPCs.CIVILIAN_785,
+        NPCs.CIVILIAN_786,
+        NPCs.CIVILIAN_787
+    )
 }
-
-
--- Server/src/main/content/region/kandarin/ardougne/west/plugin/CivilianCatPlugin.kt
+++ Server/src/main/content/region/kandarin/ardougne/west/plugin/CivilianCatPlugin.kt
@@ -1,30 +1,35 @@
 package content.region.kandarin.ardougne.west.plugin
 
-import content.region.misthalin.draynor.quest.swept_cat.grownCatItemIds
-import content.region.misthalin.draynor.quest.swept_cat.kittenItemIds
-import core.api.replaceSlot
-import core.api.sendItemDialogue
-import core.api.sendNPCDialogue
+import core.api.*
 import core.game.interaction.IntType
 import core.game.interaction.InteractionListener
-import core.game.node.item.Item
 import shared.consts.Items
 import shared.consts.NPCs
+import content.global.skill.summoning.pet.Pets
 
 class CivilianCatPlugin : InteractionListener {
     private val civilians = intArrayOf(NPCs.CIVILIAN_785, NPCs.CIVILIAN_786, NPCs.CIVILIAN_787)
 
     override fun defineListeners() {
-        onUseWith(IntType.NPC, grownCatItemIds, *civilians) { player, used, _ ->
-            player.familiarManager.removeDetails(used.idHash)
-            sendItemDialogue(player, Items.DEATH_RUNE_560, "You hand over the cat.<br>You are given 100 Death Runes.")
-            replaceSlot(player, used.asItem().slot, Item(Items.DEATH_RUNE_560, 100))
+        val GROWN_CAT = Pets.values().mapNotNull { pet -> listOfNotNull(pet.grownItemId.takeIf { it > 0 }, pet.overgrownItemId.takeIf { it > 0 }, pet.wilyItemId.takeIf { it > 0 }, pet.lazyItemId.takeIf { it > 0 }) }.flatten().toIntArray()
+        val KITTEN_ID = Pets.values().map { it.babyItemId }.toIntArray()
+
+        onUseWith(IntType.NPC, GROWN_CAT, *civilians) { player, used, _ ->
+            if(removeItem(player, used.id)) {
+                val pet = Pets.forId(used.id)
+                pet?.let {
+                    player.familiarManager.removeDetails(used.id)
+                }
+                addItem(player, Items.DEATH_RUNE_560, 100)
+                sendItemDialogue(player, Items.DEATH_RUNE_560, "You hand over the cat.<br>You are given 100 Death Runes.")
+            }
             return@onUseWith true
         }
 
-        onUseWith(IntType.NPC, kittenItemIds, *civilians) { player, _, npc ->
+        onUseWith(IntType.NPC, KITTEN_ID, *civilians) { player, _, npc ->
             sendNPCDialogue(player, npc.id, "That kitten isn't big enough; come back when it's bigger.")
             return@onUseWith true
         }
     }
+
 }
