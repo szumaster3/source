7cf1d2847ff49f8e08d191ba4170798f84198f99
szumaster <szumaster2009@protonmail.com>
2025-08-30 19:58:37 +0200

    Improve authenticity for interaction with Wendy, correct spawn locations

--- Server/src/main/content/region/misthalin/draynor/quest/swept_cat/CatOnWendyPlugin.kt
+++ Server/src/main/content/region/misthalin/draynor/quest/swept_cat/CatOnWendyPlugin.kt
@@ -1,18 +1,15 @@
 package content.region.misthalin.draynor.quest.swept_cat
 
 import content.global.skill.summoning.pet.Pets
-import core.api.openDialogue
-import core.api.isQuestComplete
-import core.api.sendMessage
+import core.api.*
 import core.game.dialogue.DialogueFile
 import core.game.dialogue.FaceAnim
 import core.game.interaction.IntType
 import core.game.interaction.InteractionListener
 import core.game.node.entity.npc.NPC
 import core.game.node.item.Item
-import shared.consts.Items
-import shared.consts.NPCs
-import shared.consts.Quests
+import core.game.world.update.flag.context.Animation
+import shared.consts.*
 
 val purpleCats = intArrayOf(
     Items.PET_KITTEN_14089,
@@ -37,6 +34,11 @@ class CatOnWendyPlugin : InteractionListener {
                 return@onUseAnyWith false
             }
 
+            if (player.familiarManager.hasPet()) {
+                sendMessage(player, "You need to pick up your pet first to do that.")
+                return@onUseAnyWith false
+            }
+
             val isPurpleCat = item.id in purpleCats
 
             if (!isQuestComplete(player, Quests.SWEPT_AWAY)) {
@@ -45,7 +47,6 @@ class CatOnWendyPlugin : InteractionListener {
             }
 
             if (isPurpleCat) {
-                sendMessage(player, "You show your purple cat to Wendy.")
                 openDialogue(player, WendyPurpleCatDialogueExtension())
             } else {
                 openDialogue(player, WendyRegularCatDialogueExtension())
@@ -65,23 +66,21 @@ class WendyRegularCatDialogueExtension : DialogueFile() {
             1 -> npcl(FaceAnim.FRIENDLY, "I can, but I should warn you: I can make cats purple, but I don't know how to change them back. It's a permanent change! Is that okay?").also { stage++ }
             2 -> options("Yes, please make my cat permanently purple.", "No, I think I'll hold off for the time being.").also { stage++ }
             3 -> when (buttonID) {
-                1 -> player("Yes, please make my cat permanently purple.").also { stage = 5 }
-                2 -> player(FaceAnim.NEUTRAL, "No, I think I'll hold off for the time being.").also { stage = 7 }
+                1 -> player("Yes, please make my cat permanently purple.").also { stage++ }
+                2 -> player(FaceAnim.NEUTRAL, "No, I think I'll hold off for the time being.").also { stage = 6 }
             }
-            5 -> {
+            4 -> {
                 val familiar = player?.familiarManager?.familiar
                 val inv = player?.inventory?.toArray() ?: arrayOfNulls<Item>(0)
                 val petID = inv.firstOrNull { it != null && Pets.forId(it.id) != null }
 
                 if (familiar == null && petID == null) {
-                    npcl(null, "Of course. Just bring your cat to me and let me hold her for a moment. I'll sort her out in no time.")
                     stage = 7
                     return
                 }
 
                 val petItemID = familiar?.id ?: petID?.id
                 val petData = Pets.forId(petItemID!!) ?: run {
-                    npcl(null, "Hmm, I can't figure out this cat.")
                     stage = 7
                     return
                 }
@@ -99,10 +98,20 @@ class WendyRegularCatDialogueExtension : DialogueFile() {
                 }
 
                 player?.familiarManager?.morphPet(Item(purpleCatID), true, familiar?.location ?: player?.location)
+
+                val newFamiliar = player?.familiarManager?.familiar
+                if (newFamiliar != null) {
+                    player?.face(newFamiliar)
+                    player?.animate(Animation(Animations.MULTI_BEND_OVER_827))
+                    sendGraphics(Graphics.PUFF_OF_GREY_1276, newFamiliar.location)
+                    newFamiliar.sendChat("Miaow!")
+                }
+
                 npcl(FaceAnim.HAPPY, "There you go! Oh, such a beautiful colour.")
-                stage = 6
+                stage = 5
             }
-            6 -> player("Thank you.").also { stage++ }
+            5 -> player("Thank you.").also { stage = 7 }
+            6 -> npcl(FaceAnim.FRIENDLY, "Okay. If you change your mind, let me know.").also { stage++ }
             7 -> end()
         }
     }
@@ -112,15 +121,16 @@ class WendyPurpleCatDialogueExtension : DialogueFile() {
     override fun handle(componentID: Int, buttonID: Int) {
         npc = NPC(NPCs.WENDY_8201)
         when (stage) {
-            0 -> npc("Oh, what a lovely cat you have there. Hello, kitty!").also { stage++ }
-            1 -> options("Can you change this cat back to its normal colour?", "Thank you.").also { stage++ }
-            2 -> when (buttonID) {
+            0 -> sendDialogue(player!!, "You show your purple cat to Wendy.").also { stage++ }
+            1 -> npc("Oh, what a lovely cat you have there. Hello, kitty!").also { stage++ }
+            2 -> options("Can you change this cat back to its normal colour?", "Thank you.").also { stage++ }
+            3 -> when (buttonID) {
                 1 -> player("Can you change this cat back to its normal colour?").also { stage++ }
-                2 -> player("Thank you.").also { stage = 5 }
+                2 -> player("Thank you.").also { stage = 6 }
             }
-            3 -> npcl(FaceAnim.STRUGGLE, "Um, did I forget to mention...once the spell is cast, it's cast for good. I can't undo the purple.").also { stage++ }
-            4 -> player(FaceAnim.HALF_CRYING, "Ah, right. Hmm.").also { stage = 5 }
-            5 -> end()
+            4 -> npcl(FaceAnim.STRUGGLE, "Um, did I forget to mention...once the spell is cast, it's cast for good. I can't undo the purple.").also { stage++ }
+            5 -> player(FaceAnim.HALF_CRYING, "Ah, right. Hmm.").also { stage = 6 }
+            6 -> end()
         }
     }
 }
