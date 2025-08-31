5d99d76d0b3056624e3055331195320d6755eef0
szumaster <szumaster2009@protonmail.com>
2025-08-31 03:38:18 +0200

    Rewrote fog reward interface, reggie dialogue and correct some examines

--- Server/src/main/content/minigame/fistofguthix/dialogue/ReggieDialogue.kt
+++ Server/src/main/content/minigame/fistofguthix/dialogue/ReggieDialogue.kt
@@ -1,11 +1,18 @@
 package content.minigame.fistofguthix.dialogue
 
-import core.game.component.Component
+import core.api.openInterface
+import core.api.setTitle
 import core.game.dialogue.Dialogue
+import core.game.dialogue.FaceAnim
+import core.game.dialogue.IfTopic
+import core.game.dialogue.Topic
+import core.game.node.entity.npc.NPC
 import core.game.node.entity.player.Player
 import core.game.node.item.Item
 import core.game.world.GameWorld
 import core.plugin.Initializable
+import shared.consts.Components
+import shared.consts.Items
 import shared.consts.NPCs
 
 /**
@@ -15,41 +22,89 @@ import shared.consts.NPCs
 class ReggieDialogue(player: Player? = null) : Dialogue(player) {
 
     override fun open(vararg args: Any?): Boolean {
-        if (GameWorld.settings?.allow_token_purchase == true) {
-            options("Can I see your shop?", "Nevermind.", "Can I buy some tokens?")
-        } else {
-            options("Can I see your shop?", "Nevermind.")
-        }
+        npc = args[0] as NPC
+        npcl(FaceAnim.HAPPY, "Welcome, adventurer. Have you come to have a look at my fine wares?")
         return true
     }
 
     override fun handle(interfaceId: Int, buttonId: Int): Boolean {
         var buyAmount = 0
         when (stage) {
-            0 -> when (buttonId) {
-                1 -> npc("Certainly!").also { stage++ }
-                2 -> end()
-                3 -> npc("Sure thing. My tokens are 1000 coins", "each.").also { stage = 10 }
+            0 -> showTopics(
+                Topic("Yes, I want to see what you're selling.", 1, true),
+                Topic("No, I have some questions.", 1,false),
+                IfTopic("I want to buy tokens.",10,GameWorld.settings?.allow_token_purchase == true)
+            )
+            1 -> npcl(FaceAnim.HAPPY, "Certainly!").also { stage++ }
+            2 -> end().also { openInterface(player, Components.FOG_REWARDS_732) }
+            3 -> showTopics(
+                Topic("What currency are you trading in?", 4, true),
+                Topic("Can you recharge my items?", 20,false),
+                Topic("Can you uncharge my items?", 30,false),
+                Topic("Why do all of your wares degrade?", 40,false),
+                Topic("No, I'm just passing by.", 100,false),
+            )
+            4 -> playerl(FaceAnim.HALF_ASKING, "What currency are you trading in? It doesn't seem to be regular coins.").also { stage++ }
+            5 -> npcl(FaceAnim.HAPPY,"You are absolutely right, my friend. I exchange my goods for so called Fist of Guthix tokens.").also { stage++ }
+            6 -> showTopics(
+                Topic("How can I get hold of these tokens?", 7, false),
+                Topic("What are Fist of Guthix tokens?", 9,false),
+                Topic("I have to go now.", 99,false)
+            )
+            7 -> npcl(FaceAnim.FRIENDLY, "If you win a game in the Fist of Guthix arena, you will be awarded with tokens, based on how well you perform.").also { stage++ }
+            8 -> showTopics(
+                Topic("What are you selling?", 2, true),
+                Topic("I have more questions that I want answered.", 3,true),
+                Topic("Thanks for the help.", 100,false)
+            )
+            9 -> npcl(FaceAnim.FRIENDLY, "Oh, they're nothing special in themselves. They are just tokens we use as currency.").also { stage = 8 }
+            10 -> {
+                setTitle(player, 4)
+                player?.dialogueInterpreter?.sendOptions("How many?", "50", "100", "250", "500").also { stage = 11 }
             }
-            1 -> end().also { player.interfaceManager.open(Component(732)) }
-            10 -> player?.dialogueInterpreter?.sendOptions("How many?", "50", "100", "250", "500").also { stage++ }
-            11 -> when (buttonId) {
-                1 -> buyAmount = 50
-                2 -> buyAmount = 100
-                3 -> buyAmount = 250
-                4 -> buyAmount = 500
-                else -> buyAmount = 0
-            }.also {
+            11 -> {
+                buyAmount = when (buttonId) {
+                    1 -> 50
+                    2 -> 100
+                    3 -> 250
+                    4 -> 500
+                    else -> 0
+                }
                 if (buyAmount > 0) {
-                    if (player.inventory?.containsItem(Item(995, 1000 * buyAmount))!!) {
-                        player.inventory?.add(Item(12852, buyAmount))
-                        player.inventory?.remove(Item(995, 1000 * buyAmount))
+                    if (player?.inventory?.containsItem(Item(Items.COINS_995, 1000 * buyAmount)) == true) {
+                        player.inventory?.add(Item(Items.FIST_OF_GUTHIX_TOKEN_12852, buyAmount))
+                        player.inventory?.remove(Item(Items.COINS_995, 1000 * buyAmount))
                     } else {
-                        player.sendMessage("You don't have enough coins for that.")
+                        player?.sendMessage("You don't have enough coins for that.")
                     }
                 }
                 end()
             }
+            20 -> npcl(FaceAnim.FRIENDLY, "By items, I assume you mean items originally acquired from me. If so, the answer is yes, my friend. I gladly spread the power of Guthix, if I am able to.").also { stage++ }
+            21 -> playerl(FaceAnim.HAPPY, "That's great.").also { stage++ }
+            22 -> npcl(FaceAnim.FRIENDLY, "It certainly is. The power of Guthix does not come cheaply, though. So, I'm afraid I will have to charge you some tokens for it. And I can only recharge items that have been completely depleted of their magical energy.").also { stage++ }
+            23 -> playerl(FaceAnim.HALF_ASKING, "Sounds fair. So, how do I go about doing this?").also { stage++ }
+            24 -> npcl(FaceAnim.FRIENDLY, "Just hand me the item you want recharged and I'll see what I can do.").also { stage++ }
+            25 -> showTopics(
+                Topic("I want to see what you're selling first.", 1, true),
+                Topic( "How can I get hold of these tokens?", 7,false),
+                Topic("I still have more questions.", 3,true),
+                Topic( "Okay, I'll do that.", 100,false)
+            )
+            30 -> npcl(FaceAnim.FRIENDLY, "You don't want to keep the power of Guthix? That's strange. But yes, of course I can do this for you. Please be aware that you won't get anything in return, except the degraded item, and it will cost you tokens to have it charged again.").also { stage++ }
+            31 -> playerl(FaceAnim.FRIENDLY, "Yes, I will keep that in mind.").also { stage++ }
+            32 -> npcl(FaceAnim.FRIENDLY, "Good. In that case, just hand me the item from which you want the charge removed.").also { stage = 25 }
+            40 -> npcl(FaceAnim.FRIENDLY, "Why do all of your wares degrade? I have seen plenty of similar items in my days, but they all seem to last a lot longer than yours.").also { stage++ }
+            41 -> npcl(FaceAnim.FRIENDLY, "A most accurate observation, my friend. But the other items you've come across in your travels most certainly have not been directly infused with the power emanating from the Fist of Guthix.").also { stage++ }
+            42 -> playerl(FaceAnim.THINKING, "Umm...I suppose not. But wouldn't the power of Guthix have made the items stronger?").also { stage++ }
+            43 -> npcl(FaceAnim.FRIENDLY, "Why, yes, they have. Keeping a shop so close to the source of this power has resulted in the items being infused with energy. This makes them stronger than they normally would have been, but, unfortunately, they are hardly suitable vessels for such divine power.").also { stage++ }
+            44 -> npcl(FaceAnim.FRIENDLY, "As they are being used, the power will be spent, and all that will remain is an empty, weakened shell. When this happens, just come back to me and I will gladly recharge them for you.").also { stage++ }
+            45 -> playerl(FaceAnim.HALF_WORRIED, "...For a small fee.").also { stage++ }
+            46 -> npcl(FaceAnim.FRIENDLY, "Haha. Yes, my friend, for a small fee. The power of a god does not come cheaply, not even in small doses. If we were to give it out for free, we would run out of resources in an instant. Balance must be maintained.").also { stage++ }
+            47 -> playerl(FaceAnim.FRIENDLY, "Okay, I will keep that in mind. Thank you.").also { stage++ }
+            48 -> npcl(FaceAnim.HALF_ASKING, "Now, is there anything else I can help you with?").also { stage = 3 }
+            99 -> playerl(FaceAnim.HALF_GUILTY, "Sorry, but I have to leave now. Goodbye.").also { stage = 100 }
+            100 -> end()
         }
         return true
     }
--- Server/src/main/content/minigame/fistofguthix/plugin/FOGRewardInterface.kt
+++ Server/src/main/content/minigame/fistofguthix/plugin/FOGRewardInterface.kt
@@ -9,127 +9,112 @@ import core.tools.Log
 import shared.consts.Components
 import shared.consts.Items
 
+/**
+ * Handles the Fist of Guthix rewards interface.
+ */
 class FOGRewardInterface : InterfaceListener {
-    class ShopItem(
-        val id: Int,
-        val price: Int,
-        val amount: Int,
-    )
+
+    /**
+     * Represents a shop stock data.
+     */
+    enum class FOGShopItem(val buttonId: Int, val itemId: Int, val price: Int, val amount: Int) {
+        DruidicMageTop(2, Items.DRUIDIC_MAGE_TOP_100_12894, 300, 1),
+        DruidicMageBottom(7, Items.DRUIDIC_MAGE_BOTTOM_100_12901, 200, 1),
+        DruidicMageHood(12, Items.DRUIDIC_MAGE_HOOD_100_12887, 100, 1),
+        CombatRobeTop(19, Items.COMBAT_ROBE_TOP_100_12971, 150, 1),
+        CombatRobeBottom(24, Items.COMBAT_ROBE_BOTTOM_100_12978, 100, 1),
+        CombatRobeHood(29, Items.COMBAT_HOOD_100_12964, 50, 1),
+        BattleRobeTop(36, Items.BATTLE_ROBE_TOP_100_12873, 1500, 1),
+        BattleRobeBottom(41, Items.BATTLE_ROBE_BOTTOM_100_12880, 1000, 1),
+        BattleRobeHood(46, Items.BATTLE_HOOD_100_12866, 250, 1),
+        GreenCoif(53, Items.GREEN_DHIDE_COIF_100_12936, 150, 1),
+        BlueCoif(58, Items.BLUE_DHIDE_COIF_100_12943, 200, 1),
+        RedCoif(63, Items.RED_DHIDE_COIF_100_12950, 300, 1),
+        BlackCoif(68, Items.BLACK_DHIDE_COIF_100_12957, 500, 1),
+        BronzeGaunt(75, Items.BRONZE_GAUNTLETS_12985, 15, 1),
+        IronGaunt(80, Items.IRON_GAUNTLETS_12988, 30, 1),
+        SteelGaunt(85, Items.STEEL_GAUNTLETS_12991, 50, 1),
+        BlackGaunt(90, Items.BLACK_GAUNTLETS_12994, 75, 1),
+        MithrilGaunt(95, Items.MITHRIL_GAUNTLETS_12997, 100, 1),
+        AdamantGaunt(100, Items.ADAMANT_GAUNTLETS_13000, 150, 1),
+        RuneGaunt(105, Items.RUNE_GAUNTLETS_13003, 200, 1),
+        DragonGaunt(110, Items.DRAGON_GAUNTLETS_13006, 300, 1),
+        AddySpike(117, Items.ADAMANT_SPIKESHIELD_100_12908, 50, 1),
+        AddyBeserk(122, Items.ADAMANT_BERSERKER_SHIELD_100_12915, 100, 1),
+        RuneSpike(127, Items.RUNE_SPIKESHIELD_100_12922, 200, 1),
+        RuneBeserk(132, Items.RUNE_BERSERKER_SHIELD_100_12929, 300, 1),
+        IritGloves(139, Items.IRIT_GLOVES_12856, 75, 1),
+        AvantoeGloves(144, Items.AVANTOE_GLOVES_12857, 100, 1),
+        KwuarmGloves(149, Items.KWUARM_GLOVES_12858, 200, 1),
+        CadantineGloves(154, Items.CADANTINE_GLOVES_12859, 200, 1),
+        SwordfishGloves(161, Items.SWORDFISH_GLOVES_12860, 200, 1),
+        SharkGloves(166, Items.SHARK_GLOVES_12861, 200, 1),
+        DragonGloves(171, Items.DRAGON_SLAYER_GLOVES_12862, 200, 1),
+        AirGloves(176, Items.AIR_RUNECRAFTING_GLOVES_12863, 75, 1),
+        WaterGloves(181, Items.WATER_RUNECRAFTING_GLOVES_12864, 75, 1),
+        EarthGloves(186, Items.EARTH_RUNECRAFTING_GLOVES_12865, 75, 1);
+
+        companion object {
+            private val map = values().associateBy(FOGShopItem::buttonId)
+            /**
+             * Gets the shop item by interface button id.
+             *
+             * @param button The button id.
+             * @return item.
+             */
+            fun fromButton(button: Int) = map[button]
+        }
+    }
 
     override fun defineInterfaceListeners() {
         on(Components.FOG_REWARDS_732) { player, _, opcode, button, _, _ ->
-            var stock = ShopItem(0, 0, 0)
-            when (button) {
-                2 -> stock = DruidicMageTop
-                7 -> stock = DruidicMageBottom
-                12 -> stock = DruidicMageHood
-                19 -> stock = CombatRobeTop
-                24 -> stock = CombatRobeBottom
-                29 -> stock = CombatRobeHood
-                36 -> stock = BattleRobeTop
-                41 -> stock = BattleRobeBottom
-                46 -> stock = BattleRobeHood
-                53 -> stock = GreenCoif
-                58 -> stock = BlueCoif
-                63 -> stock = RedCoif
-                68 -> stock = BlackCoif
-                75 -> stock = BronzeGaunt
-                80 -> stock = IronGaunt
-                85 -> stock = SteelGaunt
-                90 -> stock = BlackGaunt
-                95 -> stock = MithrilGaunt
-                100 -> stock = AdamantGaunt
-                105 -> stock = RuneGaunt
-                110 -> stock = DragonGaunt
-                117 -> stock = AddySpike
-                122 -> stock = AddyBeserk
-                127 -> stock = RuneSpike
-                132 -> stock = RuneBeserk
-                139 -> stock = IritGloves
-                144 -> stock = AvantoeGloves
-                149 -> stock = KwuarmGloves
-                154 -> stock = CadantineGloves
-                161 -> stock = SwordfishGloves
-                166 -> stock = SharkGloves
-                171 -> stock = DragonGloves
-                176 -> stock = AirGloves
-                181 -> stock = WaterGloves
-                186 -> stock = EarthGloves
-                else ->
-                    log(this::class.java, Log.WARN, "Unhandled button ID for FOG interface: $button").also {
-                        return@on true
-                    }
+            val stock = FOGShopItem.fromButton(button) ?: run {
+                log(this::class.java, Log.WARN, "Unhandled button ID for FOG interface: $button")
+                return@on true
             }
 
-            when (opcode) {
-                in listOf(196, 124, 199) -> {
-                    handlePurchase(player, stock, opcode)
-                }
-
-                155 -> sendMessage(player, "${getItemName(stock.id).replace("100", "")} costs ${stock.price} tokens.")
-                234 -> sendMessage(player, ItemDefinition.forId(stock.id).examine.replace("100", ""))
+            val action: (() -> Unit)? = when {
+                button >= 139 && opcode == 155 -> { { sendMessage(player, "${getItemName(stock.itemId).replace("100", "")} costs ${stock.price} tokens.") } }
+                button >= 139 && opcode == 124 -> { { sendMessage(player, ItemDefinition.forId(stock.itemId).examine.replace("100", "")) } }
+                opcode in listOf(196, 124, 199) -> { { handlePurchase(player, stock, opcode) } }
+                opcode == 155 -> { { sendMessage(player, "${getItemName(stock.itemId).replace("100", "")} costs ${stock.price} tokens.") } }
+                opcode == 234 -> { { sendMessage(player, ItemDefinition.forId(stock.itemId).examine.replace("100", "")) } }
+                else -> null
             }
-            true
+
+            action?.invoke()
+            return@on true
         }
     }
 
-    private fun handlePurchase(
-        player: Player,
-        stock: ShopItem,
-        opcode: Int,
-    ) {
-        val multiplier =
-            when (opcode) {
-                196 -> 1
-                124 -> 5
-                199 -> 10
-                else -> return
-            }
-
-        val neededTokens = Item(Items.FIST_OF_GUTHIX_TOKEN_12852, stock.price * multiplier)
-        if (player.inventory.containsItem(neededTokens)) {
-            if (hasSpaceFor(player, Item(stock.id, multiplier))) {
-                removeItem(player, neededTokens)
-                addItem(player, stock.id, multiplier)
-            } else {
-                sendMessage(player, "You don't have enough space in your inventory.")
-            }
+    /**
+     * Handles the purchase of a shop item.
+     *
+     * @param player The player buying the item.
+     * @param stock The shop item being purchased.
+     * @param opcode The purchase type (1x, 5x, 10x).
+     */
+    private fun handlePurchase(player: Player, stock: FOGShopItem, opcode: Int) {
+        val multiplier = when (opcode) {
+            196 -> 1
+            124 -> 5
+            199 -> 10
+            else -> return
         }
-    }
 
-    val DruidicMageTop = ShopItem(Items.DRUIDIC_MAGE_TOP_100_12894, 300, 1)
-    val DruidicMageHood = ShopItem(Items.DRUIDIC_MAGE_HOOD_100_12887, 100, 1)
-    val DruidicMageBottom = ShopItem(Items.DRUIDIC_MAGE_BOTTOM_100_12901, 200, 1)
-    val CombatRobeTop = ShopItem(Items.COMBAT_ROBE_TOP_100_12971, 150, 1)
-    val CombatRobeHood = ShopItem(Items.COMBAT_HOOD_100_12964, 50, 1)
-    val CombatRobeBottom = ShopItem(Items.COMBAT_ROBE_BOTTOM_100_12978, 100, 1)
-    val BattleRobeHood = ShopItem(Items.BATTLE_HOOD_100_12866, 250, 1)
-    val BattleRobeTop = ShopItem(Items.BATTLE_ROBE_TOP_100_12873, 1500, 1)
-    val BattleRobeBottom = ShopItem(Items.BATTLE_ROBE_BOTTOM_100_12880, 1000, 1)
-    val GreenCoif = ShopItem(Items.GREEN_DHIDE_COIF_100_12936, 150, 1)
-    val BlueCoif = ShopItem(Items.BLUE_DHIDE_COIF_100_12943, 200, 1)
-    val RedCoif = ShopItem(Items.RED_DHIDE_COIF_100_12950, 300, 1)
-    val BlackCoif = ShopItem(Items.BLACK_DHIDE_COIF_100_12957, 500, 1)
-    val BronzeGaunt = ShopItem(Items.BRONZE_GAUNTLETS_12985, 15, 1)
-    val IronGaunt = ShopItem(Items.IRON_GAUNTLETS_12988, 30, 1)
-    val SteelGaunt = ShopItem(Items.STEEL_GAUNTLETS_12991, 50, 1)
-    val BlackGaunt = ShopItem(Items.BLACK_GAUNTLETS_12994, 75, 1)
-    val MithrilGaunt = ShopItem(Items.MITHRIL_GAUNTLETS_12997, 100, 1)
-    val AdamantGaunt = ShopItem(Items.ADAMANT_GAUNTLETS_13000, 150, 1)
-    val RuneGaunt = ShopItem(Items.RUNE_GAUNTLETS_13003, 200, 1)
-    val DragonGaunt = ShopItem(Items.DRAGON_GAUNTLETS_13006, 300, 1)
-    val AddySpike = ShopItem(Items.ADAMANT_SPIKESHIELD_100_12908, 50, 1)
-    val AddyBeserk = ShopItem(Items.ADAMANT_BERSERKER_SHIELD_100_12915, 100, 1)
-    val RuneSpike = ShopItem(Items.RUNE_SPIKESHIELD_100_12922, 200, 1)
-    val RuneBeserk = ShopItem(Items.RUNE_BERSERKER_SHIELD_100_12929, 300, 1)
-    val AirGloves = ShopItem(Items.AIR_RUNECRAFTING_GLOVES_12863, 75, 1)
-    val WaterGloves = ShopItem(Items.WATER_RUNECRAFTING_GLOVES_12864, 75, 1)
-    val EarthGloves = ShopItem(Items.EARTH_RUNECRAFTING_GLOVES_12865, 75, 1)
-    val IritGloves = ShopItem(Items.IRIT_GLOVES_12856, 75, 1)
-    val AvantoeGloves = ShopItem(Items.AVANTOE_GLOVES_12857, 100, 1)
-    val KwuarmGloves = ShopItem(Items.KWUARM_GLOVES_12858, 200, 1)
-    val CadantineGloves = ShopItem(Items.CADANTINE_GLOVES_12859, 200, 1)
-    val SwordfishGloves = ShopItem(Items.SWORDFISH_GLOVES_12860, 200, 1)
-    val SharkGloves = ShopItem(Items.SHARK_GLOVES_12861, 200, 1)
-    val DragonGloves = ShopItem(Items.DRAGON_SLAYER_GLOVES_12862, 200, 1)
+        val priceItem = Item(Items.FIST_OF_GUTHIX_TOKEN_12852, stock.price * multiplier)
+
+        if (!player.inventory.containsItem(priceItem)) {
+            sendMessage(player, "You don't have enough Fist of Guthix tokens.")
+            return
+        }
+
+        if (!hasSpaceFor(player, Item(stock.itemId, multiplier))) {
+            sendMessage(player, "You don't have enough space in your inventory.")
+            return
+        }
+
+        removeItem(player, priceItem)
+        addItem(player, stock.itemId, multiplier)
+    }
 }
