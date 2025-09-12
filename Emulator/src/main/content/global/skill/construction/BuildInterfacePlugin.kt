a9a7c85af3fc1b9c862e10159e60cf566ce19ce7
szumaster <szumaster2009@protonmail.com>
2025-09-12 20:29:35 +0200

    Remove duplicate interface

--- Emulator/src/main/content/global/skill/construction/BuildInterfacePlugin.kt
+++ Emulator/src/main/content/global/skill/construction/BuildInterfacePlugin.kt
@@ -1,5 +1,6 @@
 package content.global.skill.construction
 
+import core.api.amountInInventory
 import core.cache.def.impl.ItemDefinition
 import core.game.component.Component
 import core.game.component.ComponentDefinition
@@ -12,9 +13,11 @@ import core.plugin.Plugin
 import core.tools.Log
 import shared.consts.Components
 import core.api.log
+import core.api.sendString
+import shared.consts.Items
 
 @Initializable
-class ConstructionInterface : ComponentPlugin() {
+class BuildInterfacePlugin : ComponentPlugin() {
 
     override fun newInstance(arg: Any?): Plugin<Any> {
         ComponentDefinition.put(Components.POH_BUILD_FURNITURE_396, this)
@@ -114,6 +117,12 @@ class ConstructionInterface : ComponentPlugin() {
             }
 
             Components.POH_BUILD_SCREEN_402 -> {
+                val coins = amountInInventory(player, Items.COINS_995)
+                for ((amount, childId) in COINS_VALUE_TO_CHILD) {
+                    if (coins < amount) break
+                    sendString(player, core.tools.YELLOW + "$amount coins", Components.POH_BUILD_SCREEN_402, childId)
+                }
+
                 val index = button - 160
                 log(javaClass, Log.FINE, "BuildRoom Interface Index: $index")
                 if (index in RoomProperties.values().indices) {
@@ -155,5 +164,14 @@ class ConstructionInterface : ComponentPlugin() {
             }
         }
         return true
+    }
+
+    companion object {
+        private val COINS_VALUE_TO_CHILD = arrayOf(
+            100 to 138, 5000 to 139, 7500 to 147, 7500 to 155, 7500 to 156, 7500 to 157,
+            10000 to 140, 15000 to 141, 25000 to 142, 50000 to 143, 50000 to 149, 50000 to 150,
+            75000 to 145, 75000 to 152, 100000 to 144, 100000 to 151, 150000 to 146, 150000 to 153,
+            150000 to 154, 250000 to 148, 250000 to 159
+        )
     }
 }
